a1fbf25f770337752dc6dcd8c7e736e2a69b966a
Brad Fitzpatrick <bradfitz@tailscale.com>
2020-02-10 09:00:30 -0800

    version: add stub; use rsc.io/goversion/version later

---
+++ version/version.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !redo
+
+// Package version provides the version that the binary was built at.
+package version
+
+const LONG = "LONGVER-TODO"
+const SHORT = "SHORTVER-TODO"
