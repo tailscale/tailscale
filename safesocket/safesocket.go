04e6b7777434d1c83d66e99ce1a22f213d6b30fc
Brad Fitzpatrick <bradfitz@tailscale.com>
2020-02-25 09:02:52 -0800

    safesocket: delete incorrect build tag
    Leftover from copying one file to another to create safesocket.go
    in the earlier documentation change (29f7d640910f8c37b5).

--- safesocket/safesocket.go
+++ safesocket/safesocket.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !windows
-
 // Package safesocket creates either a Unix socket, if possible, or
 // otherwise a localhost TCP connection.
 package safesocket
