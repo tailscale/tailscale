277fe84c6baeb9c9fa9e2628fc6d80fa75617f4d
Brad Fitzpatrick <brad@danga.com>
2020-04-07 09:28:09 -0700

    version: don't depend on goversion on ios

---
+++ version/cmdname_ios.go
@@ -0,0 +1,19 @@
+// Copyright (c) 2020 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build darwin,arm64
+
+package version
+
+import (
+	"os"
+)
+
+func CmdName() string {
+	e, err := os.Executable()
+	if err != nil {
+		return "cmd"
+	}
+	return e
+}
