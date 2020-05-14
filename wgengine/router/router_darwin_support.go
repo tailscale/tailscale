d53e8fc0da9210b6c05b8886502b48d934c2384b
Avery Pennarun <apenwarr@tailscale.com>
2020-05-14 04:42:36 -0400

    router_darwin_support: we can build this on every platform.
    Our new build scripts try to build ipn-go-bridge on more than just
    linux and darwin, so let's enable this file so it can be successful on
    every platform.

--- wgengine/router/router_darwin_support.go
+++ wgengine/router/router_darwin_support.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build linux darwin
-
 package router
 
 // SetRoutesFunc applies the given router settings to the OS network
