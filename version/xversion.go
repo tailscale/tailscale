04cb4abae5a00d78504356f15081963ecd78f5ee
Elias Naur <mail@eliasnaur.com>
2020-06-22 21:48:12 +0200

    version: add support for setting version with the -X Go linker flag
    Updates tailscale/tailscale#486
    
    Signed-off-by: Elias Naur <mail@eliasnaur.com>

--- version/xversion.go
+++ version/xversion.go
@@ -2,10 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !redo,!xversion
+// +build !redo,xversion
 
-// Package version provides the version that the binary was built at.
 package version
 
-const LONG = "date.20200622"
-const SHORT = LONG
+// Replaced at build time with the Go linker flag -X.
+var LONG string = "<not set>"
+var SHORT string = "<not set>"
