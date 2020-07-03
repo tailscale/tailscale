7883e5c5e762a8f2af1f2c8c44294ee0ffa99930
Brad Fitzpatrick <bradfitz@tailscale.com>
2020-07-02 22:52:42 -0700

    go.mod: restore staticcheck module, make it stick around, go mod tidy
    It kept coming & going as different people ran go mod tidy and others
    ran staticcheck.
    
    Make it stop going away with go mod tidy by adding a dep to it.

--- go.mod
+++ go.mod
@@ -28,6 +28,7 @@ require (
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
 	golang.org/x/sys v0.0.0-20200501052902-10377860bb8e
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0
+	honnef.co/go/tools v0.0.1-2020.1.4
 	inet.af/netaddr v0.0.0-20200702150737-4591d218f82c
 	rsc.io/goversion v1.2.0
 )
---
+++ tstest/staticcheck/staticcheck.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2020 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This file exists just so go mod tidy won't remove
+// staticcheck's module from our go.mod.
+
+package tstest
+
+import (
+	_ "honnef.co/go/tools/staticcheck"
+)
