19d77ce6a37a81ae2543148bd4f868c7fbda04c0
Dmytro Shynkevych <dmytro@tailscale.com>
2020-07-15 12:48:35 -0400

    cmd/tailscale: fix typo in license headers
    Signed-off-by: Dmytro Shynkevych <dmytro@tailscale.com>

--- cmd/tailscale/cli/cli.go
+++ cmd/tailscale/cli/cli.go
@@ -1,6 +1,6 @@
 // Copyright (c) 2020 Tailscale Inc & AUTHORS All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE
+// license that can be found in the LICENSE file.
 
 // Package cli contains the cmd/tailscale CLI code in a package that can be included
 // in other wrapper binaries such as the Mac and Windows clients.
