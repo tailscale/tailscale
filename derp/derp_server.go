15949ad77d0903ea145e0c4bec1cb552eca70696
David Anderson <danderson@tailscale.com>
2020-08-11 19:59:08 +0000

    derp: export the new expvar.

--- derp/derp_server.go
+++ derp/derp_server.go
@@ -1193,6 +1193,7 @@ func (s *Server) ExpVar() expvar.Var {
 	m.Set("bytes_sent", &s.bytesSent)
 	m.Set("packets_dropped", &s.packetsDropped)
 	m.Set("counter_packets_dropped_reason", &s.packetsDroppedReason)
+	m.Set("counter_packets_received_kind", &s.packetsRecvByKind)
 	m.Set("packets_sent", &s.packetsSent)
 	m.Set("packets_received", &s.packetsRecv)
 	m.Set("unknown_frames", &s.unknownFrames)
