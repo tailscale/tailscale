264c4ea0e1718059de97f7129bf97f05091aa1ea
chungdaniel <daniel@tailscale.com>
2020-08-19 18:47:11 -0400

    control/controlclient, wgengine/filter: extract parsePacketFilter to new constructor in wgengine/filter
    Signed-off-by: chungdaniel <daniel@tailscale.com>

--- wgengine/filter/filter_test.go
+++ wgengine/filter/filter_test.go
@@ -8,6 +8,7 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
+	"net"
 	"strings"
 	"testing"
 
@@ -159,6 +160,35 @@ func TestNoAllocs(t *testing.T) {
 				t.Errorf("got %d allocs per run; want at most %d", got, test.want)
 			}
 		})
+	}
+}
+
+func TestParseIP(t *testing.T) {
+	tests := []struct {
+		host    string
+		bits    int
+		want    Net
+		wantErr string
+	}{
+		{"8.8.8.8", 24, Net{IP: packet.NewIP(net.ParseIP("8.8.8.8")), Mask: packet.NewIP(net.ParseIP("255.255.255.0"))}, ""},
+		{"8.8.8.8", 33, Net{}, `invalid CIDR size 33 for host "8.8.8.8"`},
+		{"8.8.8.8", -1, Net{}, `invalid CIDR size -1 for host "8.8.8.8"`},
+		{"0.0.0.0", 24, Net{}, `ports="0.0.0.0": to allow all IP addresses, use *:port, not 0.0.0.0:port`},
+		{"*", 24, NetAny, ""},
+		{"fe80::1", 128, NetNone, `ports="fe80::1": invalid IPv4 address`},
+	}
+	for _, tt := range tests {
+		got, err := parseIP(tt.host, tt.bits)
+		if err != nil {
+			if err.Error() == tt.wantErr {
+				continue
+			}
+			t.Errorf("parseIP(%q, %v) error: %v; want error %q", tt.host, tt.bits, err, tt.wantErr)
+		}
+		if got != tt.want {
+			t.Errorf("parseIP(%q, %v) = %#v; want %#v", tt.host, tt.bits, got, tt.want)
+			continue
+		}
 	}
 }
 
