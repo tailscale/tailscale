9e2e8c80af7e388870cc4b88a65e06cd97407f49
David Crawshaw <crawshaw@tailscale.com>
2020-08-21 08:31:19 -0400

    tailcfg: more Clone methods
    Signed-off-by: David Crawshaw <crawshaw@tailscale.com>

--- tailcfg/tailcfg.go
+++ tailcfg/tailcfg.go
@@ -4,7 +4,7 @@
 
 package tailcfg
 
-//go:generate go run tailscale.com/cmd/cloner -type=User,Node,Hostinfo,NetInfo -output=tailcfg_clone.go
+//go:generate go run tailscale.com/cmd/cloner -type=User,Node,Hostinfo,NetInfo,Group,Role,Capability -output=tailcfg_clone.go
 
 import (
 	"bytes"
