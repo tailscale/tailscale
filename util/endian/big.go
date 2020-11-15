d285b548bfc64bc7ba4cd4f414479db06344efac
Brad Fitzpatrick <bradfitz@tailscale.com>
2020-11-14 19:50:00 -0800

    util/endian: add package with const for whether platform is big endian
    (cherry picked from commit 000347d4cf9077b12c6e7e02461365f21fa633b0)

---
+++ util/endian/big.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2020 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build mips mips64 ppc64 s390x
+
+package endian
+
+// Big is whether the current platform is big endian.
+const Big = true
---
+++ util/endian/little.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2020 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build 386 amd64 arm arm64 mips64le mipsle ppc64le riscv64 wasm
+
+package endian
+
+// Big is whether the current platform is big endian.
+const Big = false
