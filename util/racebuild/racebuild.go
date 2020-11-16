ff50ddf1ee7ffffbfd30d8aad780ae34024352a0
Brad Fitzpatrick <bradfitz@tailscale.com>
2020-11-16 10:11:53 -0800

    util/racebuild: add package to export a race-is-enabled const

---
+++ util/racebuild/racebuild.go
@@ -0,0 +1,7 @@
+// Copyright (c) 2020 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package racebuild exports a constant about whether the current binary
+// was built with the race detector.
+package racebuild
