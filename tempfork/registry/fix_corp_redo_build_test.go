5b338bf011d6e2500c3e81874bced0a57b6bcfbc
Brad Fitzpatrick <bradfitz@tailscale.com>
2020-11-18 08:29:38 -0800

    tempfork/registry: delete
    It's unused.

--- tempfork/registry/fix_corp_redo_build_test.go
+++
@@ -1,8 +0,0 @@
-package registry_test
-
-// Tailscale's redo-based build system doesn't know how to skip running Go tests
-// in directories that don't contain files for the current OS.
-//
-// https://github.com/tailscale/corp/issues/293
-//
-// So this is a dummy file for now to make it happy.
