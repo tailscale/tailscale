7a1813fd249646068ff70f1017dff1700aade96d
AdamKorcz <adam@adalogics.com>
2021-01-12 23:10:10 +0000

    Added 2 fuzzers
    Signed-off-by: AdamKorcz <adam@adalogics.com>

---
+++ disco/disco_fuzzer.go
@@ -0,0 +1,18 @@
+// Copyright (c) 2021 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+// +build gofuzz
+
+package disco
+
+func Fuzz(data []byte) int {
+	m, _ := Parse(data)
+
+	newBytes := m.AppendMarshal(data)
+	parsedMarshall, _ := Parse(newBytes)
+
+	if m != parsedMarshall {
+		panic("Parsing error")
+	}
+	return 1
+}
