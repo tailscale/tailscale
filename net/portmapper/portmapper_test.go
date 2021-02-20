c64bd587aee9709bc6a6203de58361e2bafc2dbf
Brad Fitzpatrick <bradfitz@tailscale.com>
2021-02-19 22:15:41 -0800

    net/portmapper: add NAT-PMP client, move port mapping service probing
    * move probing out of netcheck into new net/portmapper package
    * use PCP ANNOUNCE op codes for PCP discovery, rather than causing
      short-lived (sub-second) side effects with a 1-second-expiring map +
      delete.
    * track when we heard things from the router so we can be less wasteful
      in querying the router's port mapping services in the future
    * use portmapper from magicsock to map a public port
    
    Fixes #1298
    Fixes #1080
    Fixes #1001
    Updates #864
    
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

---
+++ net/portmapper/portmapper_test.go
@@ -0,0 +1,54 @@
+// Copyright (c) 2021 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package portmapper
+
+import (
+	"context"
+	"os"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestCreateOrGetMapping(t *testing.T) {
+	if v, _ := strconv.ParseBool(os.Getenv("HIT_NETWORK")); !v {
+		t.Skip("skipping test without HIT_NETWORK=1")
+	}
+	c := NewClient(t.Logf)
+	c.SetLocalPort(1234)
+	for i := 0; i < 2; i++ {
+		if i > 0 {
+			time.Sleep(100 * time.Millisecond)
+		}
+		ext, err := c.CreateOrGetMapping(context.Background())
+		t.Logf("Got: %v, %v", ext, err)
+	}
+}
+
+func TestClientProbe(t *testing.T) {
+	if v, _ := strconv.ParseBool(os.Getenv("HIT_NETWORK")); !v {
+		t.Skip("skipping test without HIT_NETWORK=1")
+	}
+	c := NewClient(t.Logf)
+	for i := 0; i < 2; i++ {
+		if i > 0 {
+			time.Sleep(100 * time.Millisecond)
+		}
+		res, err := c.Probe(context.Background())
+		t.Logf("Got: %+v, %v", res, err)
+	}
+}
+
+func TestClientProbeThenMap(t *testing.T) {
+	if v, _ := strconv.ParseBool(os.Getenv("HIT_NETWORK")); !v {
+		t.Skip("skipping test without HIT_NETWORK=1")
+	}
+	c := NewClient(t.Logf)
+	c.SetLocalPort(1234)
+	res, err := c.Probe(context.Background())
+	t.Logf("Probe: %+v, %v", res, err)
+	ext, err := c.CreateOrGetMapping(context.Background())
+	t.Logf("CreateOrGetMapping: %v, %v", ext, err)
+}
