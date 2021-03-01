a038e8690cc9476b8e96e82f39856c311ed73f1a
Brad Fitzpatrick <bradfitz@tailscale.com>
2021-03-01 11:19:20 -0800

    wgengine/netstack: fix 32-bit build broken from prior commit
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- wgengine/netstack/netstack_32bit.go
+++ wgengine/netstack/netstack_32bit.go
@@ -8,14 +8,30 @@
 package netstack
 
 import (
+	"context"
 	"errors"
+	"net"
 
+	"inet.af/netaddr"
 	"tailscale.com/types/logger"
+	"tailscale.com/types/netmap"
 	"tailscale.com/wgengine"
 	"tailscale.com/wgengine/magicsock"
 	"tailscale.com/wgengine/tstun"
 )
 
-func Create(logf logger.Logf, tundev *tstun.TUN, e wgengine.Engine, mc *magicsock.Conn) (wgengine.FakeImpl, error) {
+type Impl struct{}
+
+func (*Impl) Start() error { panic("noimpl") }
+
+func (*Impl) DialContextTCP(ctx context.Context, addr string) (net.Conn, error) { panic("noimpl") }
+
+type DNSMap map[string]netaddr.IP
+
+func (m DNSMap) Resolve(ctx context.Context, addr string) (netaddr.IPPort, error) { panic("noimpl") }
+
+func DNSMapFromNetworkMap(nm *netmap.NetworkMap) DNSMap { return nil }
+
+func Create(logf logger.Logf, tundev *tstun.TUN, e wgengine.Engine, mc *magicsock.Conn) (*Impl, error) {
 	return nil, errors.New("netstack is not supported on 32-bit platforms for now; see https://github.com/google/gvisor/issues/5241")
 }
