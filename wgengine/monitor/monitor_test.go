tailscale/tailscale

commit 5c1e443d348d32a206a50d92b9cea7afad881e0d

wgengine/monitor: don't call LinkChange when interfaces look unchanged
Basically, don't trust the OS-level link monitor to only tell you
interesting things. Sanity check it.

Also, move the interfaces package into the net directory now that we
have it.

wgengine/monitor/monitor.go
@@ -7,9 +7,14 @@
 package monitor
 
 import (
+	"fmt"
+	"net"
+	"runtime"
+	"strings"
 	"sync"
 	"time"
 
+	"tailscale.com/net/interfaces"
 	"tailscale.com/types/logger"
 )
 
@@ -99,6 +104,7 @@ func (m *Mon) Close() error {
 // the change channel of changes, and stopping when a stop is issued.
 func (m *Mon) pump() {
 	defer m.goroutines.Done()
+	last := interfaceSummary()
 	for {
 		_, err := m.om.Receive()
 		if err != nil {
@@ -113,9 +119,17 @@ func (m *Mon) pump() {
 			continue
 		}
 
+		cur := interfaceSummary()
+		if cur == last {
+			continue
+		}
+		m.logf("wgengine/monitor: now %v (was %v)", cur, last)
+		last = cur
+
 		select {
 		case m.change <- struct{}{}:
-		default:
+		case <-m.stop:
+			return
 		}
 	}
 }
@@ -139,4 +153,18 @@ func (m *Mon) debounce() {
 		case <-time.After(100 * time.Millisecond):
 		}
 	}
+}
+
+func interfaceSummary() string {
+	var sb strings.Builder
+	_ = interfaces.ForeachInterfaceAddress(func(ni interfaces.Interface, addr net.IP) {
+		if runtime.GOOS == "linux" && strings.HasPrefix(ni.Name, "tailscale") {
+			// Skip tailscale0, etc on Linux.
+			return
+		}
+		if ni.IsUp() {
+			fmt.Fprintf(&sb, "%s=%s ", ni.Name, addr)
+		}
+	})
+	return strings.TrimSpace(sb.String())
 }

cmd/tsshd/tsshd.go
@@ -32,7 +32,7 @@ import (
 	"github.com/gliderlabs/ssh"
 	"github.com/kr/pty"
 	gossh "golang.org/x/crypto/ssh"
-	"tailscale.com/interfaces"
+	"tailscale.com/net/interfaces"
 )
 
 var (

net/interfaces/interfaces.go
@@ -137,6 +137,36 @@ func LocalAddresses() (regular, loopback []string, err error) {
 	return regular, loopback, nil
 }
 
+// Interface is a wrapper around Go's net.Interface with some extra methods.
+type Interface struct {
+	*net.Interface
+}
+
+func (i Interface) IsLoopback() bool { return isLoopback(i.Interface) }
+func (i Interface) IsUp() bool       { return isUp(i.Interface) }
+
+// ForeachInterfaceAddress calls fn for each interface's address on the machine.
+func ForeachInterfaceAddress(fn func(Interface, net.IP)) error {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return err
+	}
+	for i := range ifaces {
+		iface := &ifaces[i]
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return err
+		}
+		for _, a := range addrs {
+			switch v := a.(type) {
+			case *net.IPNet:
+				fn(Interface{iface}, v.IP)
+			}
+		}
+	}
+	return nil
+}
+
 var cgNAT = func() *net.IPNet {
 	_, ipNet, err := net.ParseCIDR("100.64.0.0/10")
 	if err != nil {

netcheck/netcheck.go
@@ -18,8 +18,8 @@ import (
 
 	"golang.org/x/sync/errgroup"
 	"tailscale.com/derp/derpmap"
-	"tailscale.com/interfaces"
 	"tailscale.com/net/dnscache"
+	"tailscale.com/net/interfaces"
 	"tailscale.com/stun"
 	"tailscale.com/stunner"
 	"tailscale.com/types/logger"

tsweb/tsweb.go
@@ -20,8 +20,8 @@ import (
 	"strings"
 	"time"
 
-	"tailscale.com/interfaces"
 	"tailscale.com/metrics"
+	"tailscale.com/net/interfaces"
 )
 
 // DevMode controls whether extra output in shown, for when the binary is being run in dev mode.

wgengine/magicsock/magicsock.go
@@ -31,8 +31,8 @@ import (
 	"tailscale.com/derp"
 	"tailscale.com/derp/derphttp"
 	"tailscale.com/derp/derpmap"
-	"tailscale.com/interfaces"
 	"tailscale.com/net/dnscache"
+	"tailscale.com/net/interfaces"
 	"tailscale.com/netcheck"
 	"tailscale.com/stun"
 	"tailscale.com/tailcfg"

commit 3d6a344ba3d8b4a88536677fcb2ba4661ad709a8

wgengine/monitor: skip some macOS route updates, fix debounce regression
Debound was broken way back in 5c1e443d348d32 and we never noticed.

Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

wgengine/monitor/monitor.go
@@ -143,17 +143,24 @@ func (m *Mon) Close() error {
 	return err
 }
 
+func (m *Mon) stopped() bool {
+	select {
+	case <-m.stop:
+		return true
+	default:
+		return false
+	}
+}
+
 // pump continuously retrieves messages from the connection, notifying
 // the change channel of changes, and stopping when a stop is issued.
 func (m *Mon) pump() {
 	defer m.goroutines.Done()
-	for {
+	for !m.stopped() {
 		msg, err := m.om.Receive()
 		if err != nil {
-			select {
-			case <-m.stop:
+			if m.stopped() {
 				return
-			default:
 			}
 			// Keep retrying while we're not closed.
 			m.logf("error from link monitor: %v", err)
@@ -165,8 +172,10 @@ func (m *Mon) pump() {
 		}
 		select {
 		case m.change <- struct{}{}:
-		case <-m.stop:
-			return
+		default:
+			// Another change signal is already
+			// buffered. Debounce will wake up soon
+			// enough.
 		}
 	}
 }
@@ -199,7 +208,7 @@ func (m *Mon) debounce() {
 		select {
 		case <-m.stop:
 			return
-		case <-time.After(100 * time.Millisecond):
+		case <-time.After(250 * time.Millisecond):
 		}
 	}
 }

wgengine/monitor/monitor_darwin.go
@@ -51,20 +51,47 @@ func (m *darwinRouteMon) Close() error {
 }
 
 func (m *darwinRouteMon) Receive() (message, error) {
-	n, err := unix.Read(m.fd, m.buf[:])
-	if err != nil {
-		return nil, err
-	}
-	msgs, err := route.ParseRIB(route.RIBTypeRoute, m.buf[:n])
-	if err != nil {
-		m.logf("read %d bytes (% 02x), failed to parse RIB: %v", n, m.buf[:n], err)
+	for {
+		n, err := unix.Read(m.fd, m.buf[:])
+		if err != nil {
+			return nil, err
+		}
+		msgs, err := route.ParseRIB(route.RIBTypeRoute, m.buf[:n])
+		if err != nil {
+			if debugRouteMessages {
+				m.logf("read %d bytes (% 02x), failed to parse RIB: %v", n, m.buf[:n], err)
+			}
+			return unspecifiedMessage{}, nil
+		}
+		if len(msgs) == 0 {
+			if debugRouteMessages {
+				m.logf("read %d bytes with no messages (% 02x)", n, m.buf[:n])
+			}
+			continue
+		}
+		nSkip := 0
+		for _, msg := range msgs {
+			if m.skipMessage(msg) {
+				nSkip++
+			}
+		}
+		if debugRouteMessages {
+			m.logf("read %d bytes, %d messages (%d skipped)", n, len(msgs), nSkip)
+			m.logMessages(msgs)
+		}
+		if nSkip == len(msgs) {
+			continue
+		}
 		return unspecifiedMessage{}, nil
 	}
-	if debugRouteMessages {
-		m.logf("read: %d bytes, %d msgs", n, len(msgs))
-		m.logMessages(msgs)
+}
+
+func (m *darwinRouteMon) skipMessage(msg route.Message) bool {
+	switch msg.(type) {
+	case *route.InterfaceMulticastAddrMessage:
+		return true
 	}
-	return unspecifiedMessage{}, nil
+	return false
 }
 
 func (m *darwinRouteMon) logMessages(msgs []route.Message) {

wgengine/monitor/monitor_test.go
@@ -5,7 +5,10 @@
 package monitor
 
 import (
+	"flag"
 	"testing"
+
+	"tailscale.com/net/interfaces"
 )
 
 func TestMonitorStartClose(t *testing.T) {
@@ -26,5 +29,37 @@ func TestMonitorJustClose(t *testing.T) {
 	}
 	if err := mon.Close(); err != nil {
 		t.Fatal(err)
+	}
+}
+
+var monitor = flag.String("monitor", "", `go into monitor mode like 'route monitor'; test never terminates. Value can be either "raw" or "callback"`)
+
+func TestMonitorMode(t *testing.T) {
+	switch *monitor {
+	case "":
+		t.Skip("skipping non-test without --monitor")
+	case "raw", "callback":
+	default:
+		t.Skipf(`invalid --monitor value: must be "raw" or "callback"`)
+	}
+	mon, err := New(t.Logf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	switch *monitor {
+	case "raw":
+		for {
+			msg, err := mon.om.Receive()
+			if err != nil {
+				t.Fatal(err)
+			}
+			t.Logf("msg: %#v", msg)
+		}
+	case "callback":
+		mon.RegisterChangeCallback(func(changed bool, st *interfaces.State) {
+			t.Logf("cb: changed=%v, ifSt=%v", changed, st)
+		})
+		mon.Start()
+		select {}
 	}
 }