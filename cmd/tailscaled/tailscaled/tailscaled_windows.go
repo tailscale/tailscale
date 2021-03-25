82c4cb765cfba6c10a07e4dc93744ef361ffca91
Brad Fitzpatrick <bradfitz@tailscale.com>
2021-03-24 21:08:17 -0700

    cmd/tailscaled: split package main into main shim + package
    So we can empty import the guts of cmd/tailscaled from another
    module for go mod tidy reasons.
    
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- cmd/tailscaled/tailscaled/tailscaled_windows.go
+++ cmd/tailscaled/tailscaled/tailscaled_windows.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package main // import "tailscale.com/cmd/tailscaled"
+package tailscaled
 
 // TODO: check if administrator, like tswin does.
 //
