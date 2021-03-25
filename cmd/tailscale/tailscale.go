f0863346c2ee9f314da4ef3612ea59bc9e0072f6
David Crawshaw <crawshaw@tailscale.com>
2021-03-25 08:21:31 -0700

    cmd/tailscale: add web subcommand
    Used as an app frontend UI on Synology.
    
    Signed-off-by: David Crawshaw <crawshaw@tailscale.com>

--- cmd/tailscale/tailscale.go
+++ cmd/tailscale/tailscale.go
@@ -9,12 +9,18 @@ package main // import "tailscale.com/cmd/tailscale"
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"tailscale.com/cmd/tailscale/cli"
 )
 
 func main() {
-	if err := cli.Run(os.Args[1:]); err != nil {
+	args := os.Args[1:]
+	if name, _ := os.Executable(); strings.HasSuffix(filepath.Base(name), ".cgi") {
+		args = []string{"web", "-cgi"}
+	}
+	if err := cli.Run(args); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
