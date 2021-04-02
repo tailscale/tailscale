0ca04f1e01a452db909260a36f2ef1d4625cf21a
David Anderson <danderson@tailscale.com>
2021-04-01 23:14:13 -0700

    net/dns: put noop.go back, limit with build tags for staticcheck.
    Signed-off-by: David Anderson <danderson@tailscale.com>

---
+++ net/dns/noop.go
@@ -0,0 +1,19 @@
+// Copyright (c) 2020 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !linux,!freebsd,!openbsd,!windows
+
+package dns
+
+type noopManager struct{}
+
+// Up implements managerImpl.
+func (m noopManager) Up(Config) error { return nil }
+
+// Down implements managerImpl.
+func (m noopManager) Down() error { return nil }
+
+func newNoopManager(mconfig ManagerConfig) managerImpl {
+	return noopManager{}
+}
