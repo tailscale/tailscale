c5e27be1a9c1c38a82fe40bbf195bd48a24d0493
Josh Bleecher Snyder <josharian@gmail.com>
2021-04-30 15:34:03 -0700

    wgengine/wgcfg: use autogenerated Clone methods
    Delete the manually written ones named Copy.
    
    Signed-off-by: Josh Bleecher Snyder <josh@tailscale.com>

--- wgengine/wgcfg/config.go
+++ wgengine/wgcfg/config.go
@@ -10,6 +10,8 @@ import (
 	"tailscale.com/types/wgkey"
 )
 
+//go:generate go run tailscale.com/cmd/cloner -type=Config,Peer -output=clone.go
+
 // EndpointDiscoSuffix is appended to the hex representation of a peer's discovery key
 // and is then the sole wireguard endpoint for peers with a non-zero discovery key.
 // This form is then recognize by magicsock's CreateEndpoint.
@@ -31,34 +33,6 @@ type Peer struct {
 	AllowedIPs          []netaddr.IPPrefix
 	Endpoints           string // comma-separated host/port pairs: "1.2.3.4:56,[::]:80"
 	PersistentKeepalive uint16
-}
-
-// Copy makes a deep copy of Config.
-// The result aliases no memory with the original.
-func (cfg Config) Copy() Config {
-	res := cfg
-	if res.Addresses != nil {
-		res.Addresses = append([]netaddr.IPPrefix{}, res.Addresses...)
-	}
-	if res.DNS != nil {
-		res.DNS = append([]netaddr.IP{}, res.DNS...)
-	}
-	peers := make([]Peer, 0, len(res.Peers))
-	for _, peer := range res.Peers {
-		peers = append(peers, peer.Copy())
-	}
-	res.Peers = peers
-	return res
-}
-
-// Copy makes a deep copy of Peer.
-// The result aliases no memory with the original.
-func (peer Peer) Copy() Peer {
-	res := peer
-	if res.AllowedIPs != nil {
-		res.AllowedIPs = append([]netaddr.IPPrefix{}, res.AllowedIPs...)
-	}
-	return res
 }
 
 // PeerWithKey returns the Peer with key k and reports whether it was found.
