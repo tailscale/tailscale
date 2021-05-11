d707e2f7e524a994ce38615d74f1793784705232
Brad Fitzpatrick <bradfitz@tailscale.com>
2021-05-11 11:10:07 -0700

    wgengine/bench: skip flaky test
    Updates tailscale/corp#1776
    
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- wgengine/bench/bench_test.go
+++ wgengine/bench/bench_test.go
@@ -42,6 +42,7 @@ func BenchmarkBatchTCP(b *testing.B) {
 }
 
 func BenchmarkWireGuardTest(b *testing.B) {
+	b.Skip("flaky; see https://github.com/tailscale/corp/issues/1776")
 	run(b, func(logf logger.Logf, traf *TrafficGen) {
 		setupWGTest(b, logf, traf, Addr1, Addr2)
 	})
