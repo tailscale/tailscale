314d15b3fbdd3924cfd4f70f115fbac2c3070094
Brad Fitzpatrick <bradfitz@tailscale.com>
2021-05-12 13:12:41 -0700

    version: add func IsRace to report whether race detector enabled
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

---
+++ version/race_off.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !race
+
+package version
+
+// IsRace reports whether the current binary was built with the Go
+// race detector enabled.
+func IsRace() bool { return false }
