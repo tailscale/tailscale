3bef426e4ca7f5f6872a079546a86e332fae3ce0
Aaditya Chaudhary <32117362+AadityaChaudhary@users.noreply.github.com>
2021-05-31 16:45:49 -0400

    Implemented Commandline Download Speedtest
    Added the net/speedtest package that contains code for starting up a
    speedtest server and a client.The speedtest command for starting a
    client takes in parameters for the speedtest as well as the host and
    port of the speedtest server to connect to. The speedtest command for
    starting a server takes in a host:port pair to listen on.
    
    Signed-off-by: Aaditya Chaudhary <32117362+AadityaChaudhary@users.noreply.github.com>

---
+++ net/speedtest/speedtest_client.go
@@ -0,0 +1,42 @@
+// Copyright (c) 2021 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package speedtest
+
+import (
+	"encoding/json"
+	"errors"
+	"net"
+	"time"
+)
+
+// RunClient dials the given address and starts a speedtest.
+// It returns any errors that come up in the tests.
+// If there are no errors in the test, it returns a slice of results.
+func RunClient(direction Direction, duration time.Duration, host string) ([]Result, error) {
+	conn, err := net.Dial("tcp", host)
+	if err != nil {
+		return nil, err
+	}
+
+	conf := config{TestDuration: duration, Version: version, Direction: direction}
+
+	defer conn.Close()
+	encoder := json.NewEncoder(conn)
+
+	if err = encoder.Encode(conf); err != nil {
+		return nil, err
+	}
+
+	var response configResponse
+	decoder := json.NewDecoder(conn)
+	if err = decoder.Decode(&response); err != nil {
+		return nil, err
+	}
+	if response.Error != "" {
+		return nil, errors.New(response.Error)
+	}
+
+	return doTest(conn, conf)
+}
