283614d5e96cc5625caeb18551b03ac31b7dd388
Josh Bleecher Snyder <josh@tailscale.com>
2021-06-03 12:02:24 -0700

    use io_uring for sendmsg
    and clean up some dead code and unify some things

--- net/uring/io_uring.c
+++ net/uring/io_uring.c
@@ -48,39 +48,20 @@ static uint64_t packNIdx(int n, size_t idx) {
     return (n64 << 32) | idx64;
 }
 
-// Wait for a completion to be available, fetch the data
-static uint64_t receive_into_udp(struct io_uring *ring) {
-    struct io_uring_cqe *cqe;
-again:;
-
-    int ret = io_uring_wait_cqe(ring, &cqe);
-    if (ret == -EINTR) {
-        goto again;
-    }
-    // TODO: Delete perror, fprintf, etc.
-    // Encode in return value or similar.
-    if (ret < 0) {
-        perror("io_uring_wait_cqe");
-        return ret;
-    }
-    int n = cqe->res;
-    if (n < 0) {
-        // TODO: this leaks a buffer!!!!
-        fprintf(stderr, "recvmsg failed: %d.\n", n);
-        return n;
-    }
-    size_t idx = (size_t)io_uring_cqe_get_data(cqe);
-    uint64_t nidx = packNIdx(n, idx);
-    io_uring_cqe_seen(ring, cqe);
-    return nidx;
-}
-
 static uint32_t ip(struct sockaddr_in *sa) {
     return ntohl(sa->sin_addr.s_addr);
 }
 
 static uint16_t port(struct sockaddr_in *sa) {
     return ntohs(sa->sin_port);
+}
+
+static uint32_t setIP(struct sockaddr_in *sa, uint32_t ip) {
+    sa->sin_addr.s_addr = htonl(ip);
+}
+
+static uint16_t setPort(struct sockaddr_in *sa, uint16_t port) {
+    sa->sin_port = htons(port);
 }
 
 // submit a recvmsg request via liburing
@@ -103,6 +84,26 @@ static int submit_recvmsg_request(struct io_uring *ring, struct msghdr *mhdr, st
     return 0;
 }
 
+// submit a recvmsg request via liburing
+// TODO: What recvfrom support arrives, maybe use that instead?
+static int submit_sendmsg_request(struct io_uring *ring, struct msghdr *mhdr, struct iovec *iov, struct sockaddr_in *sender, char *buf, int buflen, size_t idx) {
+    iov->iov_base = buf;
+    iov->iov_len = buflen;
+
+    mhdr->msg_iov = iov;
+    mhdr->msg_iovlen = 1;
+
+    mhdr->msg_name = sender;
+    mhdr->msg_namelen = sizeof(struct sockaddr_in);
+
+    struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
+    io_uring_prep_sendmsg(sqe, 0, mhdr, 0); // use the 0th file in the list of registered fds
+    io_uring_sqe_set_flags(sqe, IOSQE_FIXED_FILE);
+    io_uring_sqe_set_data(sqe, (void *)(idx));
+    io_uring_submit(ring);
+    return 0;
+}
+
 static void submit_nop_request(struct io_uring *ring) {
     struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
 	io_uring_prep_nop(sqe);
@@ -111,8 +112,7 @@ static void submit_nop_request(struct io_uring *ring) {
 }
 
 // Wait for a completion to be available, fetch the data
-// TODO: unify with receive_into_udp
-static uint64_t get_file_completion(struct io_uring *ring) {
+static uint64_t wait_completion(struct io_uring *ring) {
     struct io_uring_cqe *cqe;
 again:;
 
@@ -123,13 +123,13 @@ again:;
     // TODO: Delete perror, fprintf, etc.
     // Encode in return value or similar.
     if (ret < 0) {
-        perror("get_file_completion io_uring_wait_cqe");
+        perror("wait_completion io_uring_wait_cqe");
         return ret;
     }
     int n = cqe->res;
     if (n < 0) {
         // TODO: This leaks a buffer!!!
-        fprintf(stderr, "get_file_completion write failed: %d.\n", n);
+        fprintf(stderr, "wait_completion failed: %d.\n", n);
         return n;
     }
     size_t idx = (size_t)io_uring_cqe_get_data(cqe);
@@ -140,12 +140,6 @@ again:;
 
 // submit a write request via liburing
 static int submit_write_request(struct io_uring *ring, char *buf, int buflen, size_t idx, struct iovec *iov) {
-    // fprintf(stderr, "submit_write_request to fd %d buf %p %s buflen %d idx %lu\n", fd, buf, buf, buflen, idx);
-    // errno= 0;
-    // perror("before bonus write");
-    // int x = write(fd, buf, buflen);
-    // fprintf(stderr, "plain write returned %d\n", x);
-    // perror("submit_write_request bonus write");
     iov->iov_base = buf;
     iov->iov_len = buflen;
 
@@ -154,15 +148,12 @@ static int submit_write_request(struct io_uring *ring, char *buf, int buflen, si
     io_uring_sqe_set_flags(sqe, IOSQE_FIXED_FILE);
     io_uring_sqe_set_data(sqe, (void *)(idx));
     int submitted = io_uring_submit(ring);
-    // fprintf(stderr, "submitted %d sqes\n", submitted);
     return 0;
 }
 
-// TODO: unify with get_file_completion
-static uint64_t peek_file_completion(struct io_uring *ring) {
+static uint64_t peek_completion(struct io_uring *ring) {
     struct io_uring_cqe *cqe;
     int ret = io_uring_peek_cqe(ring, &cqe);
-    // perror("on entry, peek_file_completion io_uring_wait_cqe");
     if ((-ret == EAGAIN) || (-ret == EINTR)) {
         return ret;
     }
