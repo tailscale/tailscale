83742afabfc8c0e0d39c5bd541a27bc7b50247da
Josh Bleecher Snyder <josh@tailscale.com>
2021-07-06 13:16:24 -0700

    convert manual tests into automated tests

---
+++ net/uring/file_test.go
@@ -0,0 +1,33 @@
+package uring
+
+import (
+	"os"
+	"testing"
+
+	qt "github.com/frankban/quicktest"
+)
+
+func TestFileRead(t *testing.T) {
+	if !Available() {
+		t.Skip("io_uring not available")
+	}
+	c := qt.New(t)
+
+	const path = "testdata/voltaire.txt"
+	want, err := os.ReadFile(path)
+	c.Assert(err, qt.IsNil)
+
+	f, err := os.Open(path)
+	c.Assert(err, qt.IsNil)
+	t.Cleanup(func() { f.Close() })
+
+	uf, err := NewFile(f)
+	if err != nil {
+		t.Skipf("io_uring not available: %v", err)
+	}
+	t.Cleanup(func() { uf.Close() })
+	buf := make([]byte, len(want)+128)
+	n, err := uf.Read(buf)
+	c.Assert(err, qt.IsNil)
+	c.Assert(buf[:n], qt.DeepEquals, want)
+}
