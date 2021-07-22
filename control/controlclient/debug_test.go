1986d071c39f63cedc7892a9c5c931889123f151
Brad Fitzpatrick <bradfitz@tailscale.com>
2021-07-22 12:25:01 -0700

    control/controlclient: don't use regexp in goroutine stack scrubbing
    To reduce binary size on iOS.
    
    Updates tailscale/corp#2238
    
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- control/controlclient/debug_test.go
+++ control/controlclient/debug_test.go
@@ -9,3 +9,22 @@ import "testing"
 func TestScrubbedGoroutineDump(t *testing.T) {
 	t.Logf("Got:\n%s\n", scrubbedGoroutineDump())
 }
+
+func TestScrubHex(t *testing.T) {
+	tests := []struct {
+		in, want string
+	}{
+		{"foo", "foo"},
+		{"", ""},
+		{"0x", "?_"},
+		{"0x001 and same 0x001", "v1%1_ and same v1%1_"},
+		{"0x008 and same 0x008", "v1%0_ and same v1%0_"},
+		{"0x001 and diff 0x002", "v1%1_ and diff v2%2_"},
+	}
+	for _, tt := range tests {
+		got := scrubHex([]byte(tt.in))
+		if string(got) != tt.want {
+			t.Errorf("for input:\n%s\n\ngot:\n%s\n\nwant:\n%s\n", tt.in, got, tt.want)
+		}
+	}
+}
