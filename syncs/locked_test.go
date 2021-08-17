d2aa144dcc06ca2798f3027ea84944c0c2ad5678
Josh Bleecher Snyder <josh@tailscale.com>
2021-08-17 10:20:24 -0700

    syncs: bump known good version to include Go 1.17
    Signed-off-by: Josh Bleecher Snyder <josh@tailscale.com>

--- syncs/locked_test.go
+++ syncs/locked_test.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build go1.13 && !go1.16
-// +build go1.13,!go1.16
+//go:build go1.13 && !go1.18
+// +build go1.13,!go1.18
 
 package syncs
 
