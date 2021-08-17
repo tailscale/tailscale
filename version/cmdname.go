6da6d47a8311e63e5234c54d5179bd35d285b57a
Josh Bleecher Snyder <josh@tailscale.com>
2021-08-17 10:25:32 -0700

    all: simplify build tags involving iOS
    Prior to Go 1.16, iOS used GOOS=darwin,
    so we had to distinguish macOS from iOS during GOARCH.
    
    We now require Go 1.16 in our go.mod, so we can simplify.
    
    Signed-off-by: Josh Bleecher Snyder <josh@tailscale.com>

--- version/cmdname.go
+++ version/cmdname.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build (go1.16 && !ios) || (!go1.16 && !darwin) || (!go1.16 && !arm64)
-// +build go1.16,!ios !go1.16,!darwin !go1.16,!arm64
+//go:build !ios
+// +build !ios
 
 package version
 
