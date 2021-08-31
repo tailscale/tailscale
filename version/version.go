8744394cde2c96c46d5d3256bcb8af5201559102
Brad Fitzpatrick <bradfitz@tailscale.com>
2021-08-31 15:21:06 -0700

    version: bump date
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- version/version.go
+++ version/version.go
@@ -8,7 +8,7 @@ package version
 // Long is a full version number for this build, of the form
 // "x.y.z-commithash", or "date.yyyymmdd" if no actual version was
 // provided.
-var Long = "date.20210823"
+var Long = "date.20210831"
 
 // Short is a short version number for this build, of the form
 // "x.y.z", or "date.yyyymmdd" if no actual version was provided.
