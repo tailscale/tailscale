883db03d353fefd53ee0c8b6e7be18090f76db84
David Crawshaw <crawshaw@tailscale.com>
2021-08-31 16:36:01 -0700

    tstest/integration/vms: move build tags from linux to !windows
    The tests build fine on other Unix's, they just can't run there.
    But there is already a t.Skip by default, so `go test` ends up
    working fine elsewhere and checks the code compiles.
    
    Signed-off-by: David Crawshaw <crawshaw@tailscale.com>

--- tstest/integration/vms/top_level_test.go
+++ tstest/integration/vms/top_level_test.go
@@ -2,20 +2,19 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build linux
-// +build linux
+//go:build !windows
+// +build !windows
 
 package vms
 
 import "testing"
 
 func TestRunUbuntu1804(t *testing.T) {
-	t.Parallel()
 	setupTests(t)
 	testOneDistribution(t, 0, Distros[0])
 }
+
 func TestRunUbuntu2004(t *testing.T) {
-	t.Parallel()
 	setupTests(t)
 	testOneDistribution(t, 1, Distros[1])
 }
