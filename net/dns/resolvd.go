065236134413a52b33d52d1a2546197c9a8cede3
Aaron Bieber <aaron@bolddaemon.com>
2021-09-02 16:15:21 -0600

    net/dns: teach OpenBSD's manager to talk to resolvd(8).
    OpenBSD 6.9 and up has a daemon which handles nameserver configuration. This MR
    teaches the OpenBSD dns manager to check if resolvd is being used. If it is, it
    will use the route(8) command to tell resolvd to add the Tailscale dns entries
    to resolv.conf
    
    Signed-off-by: Aaron Bieber <aaron@bolddaemon.com>

---
+++ net/dns/resolvd.go
@@ -0,0 +1,53 @@
+// Copyright (c) 2020 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build openbsd
+// +build openbsd
+
+package dns
+
+import (
+	"os/exec"
+
+	"tailscale.com/types/logger"
+)
+
+func newResolvdManager(logf logger.Logf, interfaceName string) (*resolvdManager, error) {
+	return &resolvdManager{
+		logf:   logf,
+		ifName: interfaceName,
+	}, nil
+}
+
+// resolvdManager is an OSConfigurator which uses route(1) to teach OpenBSD's
+// resolvd(8) about DNS servers.
+type resolvdManager struct {
+	logf   logger.Logf
+	ifName string
+}
+
+func (m *resolvdManager) SetDNS(config OSConfig) error {
+	args := []string{
+		"nameserver",
+		m.ifName,
+	}
+
+	for _, s := range config.Nameservers {
+		args = append(args, s.String())
+	}
+
+	cmd := exec.Command("/sbin/route", args...)
+	return cmd.Run()
+}
+func (m *resolvdManager) SupportsSplitDNS() bool {
+	return true
+}
+
+func (m *resolvdManager) GetBaseConfig() (OSConfig, error) {
+	return OSConfig{}, ErrGetBaseConfigNotSupported
+}
+
+func (m *resolvdManager) Close() error {
+	return nil
+}
