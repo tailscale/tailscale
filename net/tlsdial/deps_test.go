09c2462ae53956bc824cd3a47ddaaa21d05e5d6d
Brad Fitzpatrick <bradfitz@tailscale.com>
2021-10-01 10:30:01 -0700

    net/tlsdial: add forgotten test file for go mod tidy
    I forgot to include this file in the earlier
    7cf8ec8108f4e26092bf07f698c8c5ce70d27d13 commit.
    
    This exists purely to keep "go mod tidy" happy.
    
    Updates #1609
    
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

---
+++ net/tlsdial/deps_test.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build for_go_mod_tidy_only
+// +build for_go_mod_tidy_only
+
+package tlsdial
+
+import _ "filippo.io/mkcert"
