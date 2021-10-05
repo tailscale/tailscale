0410f1a35a32dc3f09d8bc7d67a6845291a0d8b5
Brad Fitzpatrick <bradfitz@tailscale.com>
2021-10-05 15:32:17 -0700

    util/groupmember: adjust build tags for osusergo
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- util/groupmember/groupmember_cgo.go
+++ util/groupmember/groupmember_cgo.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build cgo
-// +build cgo
+//go:build cgo && !osusergo
+// +build cgo,!osusergo
 
 package groupmember
 
