2a0d3f72c55bb5b3b2a9bd7c836fc6294de1762b
Brad Fitzpatrick <bradfitz@tailscale.com>
2021-10-05 15:21:18 -0700

    util/groupmember: fix typo of package's name in its package doc
    And canonicalize a func comment while I'm here.
    
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- util/groupmember/groupmember.go
+++ util/groupmember/groupmember.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package groupmemeber verifies group membership of the provided user on the
+// Package groupmember verifies group membership of the provided user on the
 // local system.
 package groupmember
 
@@ -13,9 +13,8 @@ import (
 
 var ErrNotImplemented = errors.New("not implemented for GOOS=" + runtime.GOOS)
 
-// IsMemberOfGroup verifies if the provided user is member of the provided
-// system group.
-// If verification fails, an error is returned.
+// IsMemberOfGroup reports whether the provided user is a member of
+// the provided system group.
 func IsMemberOfGroup(group, userName string) (bool, error) {
 	return isMemberOfGroup(group, userName)
 }
