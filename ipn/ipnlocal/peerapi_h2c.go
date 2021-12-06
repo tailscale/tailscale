5b18ed64adf8f829d5201e17908eadac7672583b
Brad Fitzpatrick <bradfitz@tailscale.com>
2021-12-06 14:58:29 -0800

    ipn/ipnlocal: add HTTP/2 h2c server support to peerapi on non-mobile platforms
    To make ExitDNS cheaper.
    
    Might not finish client-side support in December before 1.20, but at
    least server support can start rolling out ahead of clients being
    ready for it.
    
    Tested with curl against peerapi.
    
    Updates #1713
    
    Change-Id: I676fed5fb1aef67e78c542a3bc93bddd04dd11fe
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

---
+++ ipn/ipnlocal/peerapi_h2c.go
@@ -0,0 +1,22 @@
+// Copyright (c) 2021 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build !ios && !android
+// +build !ios,!android
+
+package ipnlocal
+
+import (
+	"net/http"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+func init() {
+	addH2C = func(s *http.Server) {
+		h2s := &http2.Server{}
+		s.Handler = h2c.NewHandler(s.Handler, h2s)
+	}
+}
