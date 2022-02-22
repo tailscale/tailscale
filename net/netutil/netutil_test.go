tailscale/tailscale

commit 8fe503057da26a2e29db6ca27d2ef4573ae62395

net/netutil: unify two oneConnListeners into a new package
I was about to add a third copy, so unify them now instead.

Change-Id: I3b93896aa1249b1250a6b1df4829d57717f2311a
Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

ipn/ipnlocal/peerapi.go
@@ -38,6 +38,7 @@ import (
 	"tailscale.com/logtail/backoff"
 	"tailscale.com/net/dns/resolver"
 	"tailscale.com/net/interfaces"
+	"tailscale.com/net/netutil"
 	"tailscale.com/syncs"
 	"tailscale.com/tailcfg"
 	"tailscale.com/util/clientmetric"
@@ -506,26 +507,8 @@ func (pln *peerAPIListener) ServeConn(src netaddr.IPPort, c net.Conn) {
 	if addH2C != nil {
 		addH2C(httpServer)
 	}
-	go httpServer.Serve(&oneConnListener{Listener: pln.ln, conn: c})
+	go httpServer.Serve(netutil.NewOneConnListenerFrom(c, pln.ln))
 }
-
-type oneConnListener struct {
-	net.Listener
-	conn net.Conn
-}
-
-func (l *oneConnListener) Accept() (c net.Conn, err error) {
-	c = l.conn
-	if c == nil {
-		err = io.EOF
-		return
-	}
-	err = nil
-	l.conn = nil
-	return
-}
-
-func (l *oneConnListener) Close() error { return nil }
 
 // peerAPIHandler serves the Peer API for a source specific client.
 type peerAPIHandler struct {

ipn/ipnserver/server.go
@@ -39,6 +39,7 @@ import (
 	"tailscale.com/ipn/store/aws"
 	"tailscale.com/logtail/backoff"
 	"tailscale.com/net/netstat"
+	"tailscale.com/net/netutil"
 	"tailscale.com/net/tsdial"
 	"tailscale.com/paths"
 	"tailscale.com/safesocket"
@@ -308,7 +309,7 @@ func (s *Server) serveConn(ctx context.Context, c net.Conn, logf logger.Logf) {
 			ErrorLog:    logger.StdLogger(logf),
 			Handler:     s.localhostHandler(ci),
 		}
-		httpServer.Serve(&oneConnListener{&protoSwitchConn{s: s, br: br, Conn: c}})
+		httpServer.Serve(netutil.NewOneConnListener(&protoSwitchConn{s: s, br: br, Conn: c}))
 		return
 	}
 
@@ -1060,29 +1061,6 @@ func getEngineUntilItWorksWrapper(getEngine func() (wgengine.Engine, error)) fun
 		return e, nil
 	}
 }
-
-type dummyAddr string
-type oneConnListener struct {
-	conn net.Conn
-}
-
-func (l *oneConnListener) Accept() (c net.Conn, err error) {
-	c = l.conn
-	if c == nil {
-		err = io.EOF
-		return
-	}
-	err = nil
-	l.conn = nil
-	return
-}
-
-func (l *oneConnListener) Close() error { return nil }
-
-func (l *oneConnListener) Addr() net.Addr { return dummyAddr("unused-address") }
-
-func (a dummyAddr) Network() string { return string(a) }
-func (a dummyAddr) String() string  { return string(a) }
 
 // protoSwitchConn is a net.Conn that's we want to speak HTTP to but
 // it's already had a few bytes read from it to determine that it's

net/netutil/netutil.go
@@ -1,0 +1,50 @@
+// Copyright (c) 2022 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package netutil contains misc shared networking code & types.
+package netutil
+
+import (
+	"io"
+	"net"
+)
+
+// NewOneConnListener returns a net.Listener that returns c on its first
+// Accept and EOF thereafter. The Listener's Addr is a dummy address.
+func NewOneConnListener(c net.Conn) net.Listener {
+	return NewOneConnListenerFrom(c, dummyListener{})
+}
+
+// NewOneConnListenerFrom returns a net.Listener wrapping ln where
+// its Accept returns c on the first call and io.EOF thereafter.
+func NewOneConnListenerFrom(c net.Conn, ln net.Listener) net.Listener {
+	return &oneConnListener{c, ln}
+}
+
+type oneConnListener struct {
+	conn net.Conn
+	net.Listener
+}
+
+func (l *oneConnListener) Accept() (c net.Conn, err error) {
+	c = l.conn
+	if c == nil {
+		err = io.EOF
+		return
+	}
+	err = nil
+	l.conn = nil
+	return
+}
+
+type dummyListener struct{}
+
+func (dummyListener) Close() error                    { return nil }
+func (dummyListener) Addr() net.Addr                  { return dummyAddr("unused-address") }
+func (dummyListener) Accept() (c net.Conn, err error) { return nil, io.EOF }
+
+type dummyAddr string
+
+func (a dummyAddr) Network() string { return string(a) }
+func (a dummyAddr) String() string  { return string(a) }

commit ef782a473614dd5a9278b2c0bd603cfd4c4401a6

net/netutil: fix regression where peerapi would get closed after 1st req
I introduced a bug in 8fe503057da26 when unifying oneConnListener
implementations.

The NewOneConnListenerFrom API was easy to misuse (its Close method
closes the underlying Listener), and we did (via http.Serve, which
closes the listener after use, which meant we were close the peerapi's
listener, even though we only wanted its Addr)

Instead, combine those two constructors into one and pass in the Addr
explicitly, without delegating through to any Listener.

Change-Id: I061d7e5f842e0cada416e7b2dd62100d4f987125
Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

ipn/ipnlocal/peerapi.go
@@ -411,7 +411,7 @@ func (s *peerAPIServer) listen(ip netaddr.IP, ifState *interfaces.State) (ln net
 	}
 
 	// Make a best effort to pick a deterministic port number for
-	// the ip The lower three bytes are the same for IPv4 and IPv6
+	// the ip. The lower three bytes are the same for IPv4 and IPv6
 	// Tailscale addresses (at least currently), so we'll usually
 	// get the same port number on both address families for
 	// dev/debugging purposes, which is nice. But it's not so
@@ -507,7 +507,7 @@ func (pln *peerAPIListener) ServeConn(src netaddr.IPPort, c net.Conn) {
 	if addH2C != nil {
 		addH2C(httpServer)
 	}
-	go httpServer.Serve(netutil.NewOneConnListenerFrom(c, pln.ln))
+	go httpServer.Serve(netutil.NewOneConnListener(c, pln.ln.Addr()))
 }
 
 // peerAPIHandler serves the Peer API for a source specific client.

ipn/ipnserver/server.go
@@ -312,7 +312,7 @@ func (s *Server) serveConn(ctx context.Context, c net.Conn, logf logger.Logf) {
 			ErrorLog:    logger.StdLogger(logf),
 			Handler:     s.localhostHandler(ci),
 		}
-		httpServer.Serve(netutil.NewOneConnListener(&protoSwitchConn{s: s, br: br, Conn: c}))
+		httpServer.Serve(netutil.NewOneConnListener(&protoSwitchConn{s: s, br: br, Conn: c}, nil))
 		return
 	}
 

net/netutil/netutil.go
@@ -8,34 +8,49 @@ package netutil
 import (
 	"io"
 	"net"
+	"sync"
 )
 
-// NewOneConnListener returns a net.Listener that returns c on its first
-// Accept and EOF thereafter. The Listener's Addr is a dummy address.
-func NewOneConnListener(c net.Conn) net.Listener {
-	return NewOneConnListenerFrom(c, dummyListener{})
-}
-
-// NewOneConnListenerFrom returns a net.Listener wrapping ln where
-// its Accept returns c on the first call and io.EOF thereafter.
-func NewOneConnListenerFrom(c net.Conn, ln net.Listener) net.Listener {
-	return &oneConnListener{c, ln}
+// NewOneConnListener returns a net.Listener that returns c on its
+// first Accept and EOF thereafter.
+//
+// The returned Listener's Addr method returns addr if non-nil. If nil,
+// Addr returns a non-nil dummy address instead.
+func NewOneConnListener(c net.Conn, addr net.Addr) net.Listener {
+	if addr == nil {
+		addr = dummyAddr("one-conn-listener")
+	}
+	return &oneConnListener{
+		addr: addr,
+		conn: c,
+	}
 }
 
 type oneConnListener struct {
+	addr net.Addr
+
+	mu   sync.Mutex
 	conn net.Conn
-	net.Listener
 }
 
-func (l *oneConnListener) Accept() (c net.Conn, err error) {
-	c = l.conn
+func (ln *oneConnListener) Accept() (c net.Conn, err error) {
+	ln.mu.Lock()
+	defer ln.mu.Unlock()
+	c = ln.conn
 	if c == nil {
 		err = io.EOF
 		return
 	}
 	err = nil
-	l.conn = nil
+	ln.conn = nil
 	return
+}
+
+func (ln *oneConnListener) Addr() net.Addr { return ln.addr }
+
+func (ln *oneConnListener) Close() error {
+	ln.Accept() // guarantee future call returns io.EOF
+	return nil
 }
 
 type dummyListener struct{}

net/netutil/netutil_test.go
@@ -1,0 +1,54 @@
+// Copyright (c) 2022 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package netutil
+
+import (
+	"io"
+	"net"
+	"testing"
+)
+
+type conn struct {
+	net.Conn
+}
+
+func TestOneConnListener(t *testing.T) {
+	c1 := new(conn)
+	a1 := dummyAddr("a1")
+
+	// Two Accepts
+	ln := NewOneConnListener(c1, a1)
+	if got := ln.Addr(); got != a1 {
+		t.Errorf("Addr = %#v; want %#v", got, a1)
+	}
+	c, err := ln.Accept()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if c != c1 {
+		t.Fatalf("didn't get c1; got %p", c)
+	}
+	c, err = ln.Accept()
+	if err != io.EOF {
+		t.Errorf("got %v; want EOF", err)
+	}
+	if c != nil {
+		t.Errorf("unexpected non-nil Conn")
+	}
+
+	// Close before Accept
+	ln = NewOneConnListener(c1, a1)
+	ln.Close()
+	_, err = ln.Accept()
+	if err != io.EOF {
+		t.Fatalf("got %v; want EOF", err)
+	}
+
+	// Implicit addr
+	ln = NewOneConnListener(c1, nil)
+	if ln.Addr() == nil {
+		t.Errorf("nil Addr")
+	}
+}