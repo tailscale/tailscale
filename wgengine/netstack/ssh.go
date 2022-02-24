ef4c0e5e54e7e7cfd64ccafeb2c7bb9e891033b3
Brad Fitzpatrick <bradfitz@tailscale.com>
2022-02-24 12:27:42 -0800

    ssh: make it build on darwin
    For local dev testing initially. Product-wise, it'll probably only be
    workable on the two unsandboxed builds.
    
    Updates #3802
    
    Change-Id: Ic352f966e7fb29aff897217d79b383131bf3f92b
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- wgengine/netstack/ssh.go
+++ wgengine/netstack/ssh.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build linux
-// +build linux
+//go:build linux || (darwin && !ios)
+// +build linux darwin,!ios
 
 package netstack
 
