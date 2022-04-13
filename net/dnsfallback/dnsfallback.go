a49d8d52006eaed7c08dbaa411b92ae3f3363446
Brad Fitzpatrick <bradfitz@tailscale.com>
2022-04-13 13:49:18 -0700

    Revert ".github/workflows: work around golang/go#51629"
    This reverts commit 2a412ac9eeca4d7fca7660543df2dbe80868bf99.
    
    Updates #4194
    
    Change-Id: I0098b66b71d20bea301ca79058c1cdd201237dd0
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- net/dnsfallback/dnsfallback.go
+++ net/dnsfallback/dnsfallback.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:generate go run update-dns-fallbacks.go
+
 // Package dnsfallback contains a DNS fallback mechanism
 // for starting up Tailscale when the system DNS is broken or otherwise unavailable.
 package dnsfallback
