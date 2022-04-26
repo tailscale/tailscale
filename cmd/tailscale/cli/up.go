928d1fddd2d147823eaeaaaa58ff8b943c7bcabf
James Tucker <james@tailscale.com>
2022-04-25 17:14:18 -0700

    cmd/tailscale: s/-authkey/-auth-key/ in help text
    Signed-off-by: James Tucker <james@tailscale.com>

--- cmd/tailscale/cli/up.go
+++ cmd/tailscale/cli/up.go
@@ -52,7 +52,7 @@ down").
 If flags are specified, the flags must be the complete set of desired
 settings. An error is returned if any setting would be changed as a
 result of an unspecified flag's default value, unless the --reset flag
-is also used. (The flags --authkey, --force-reauth, and --qr are not
+is also used. (The flags --auth-key, --force-reauth, and --qr are not
 considered settings that need to be re-specified when modifying
 settings.)
 `),
