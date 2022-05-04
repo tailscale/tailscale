b3ff113c6b8f2d3b84bb11da2c1db1f98734b716
Brad Fitzpatrick <bradfitz@tailscale.com>
2022-05-03 21:42:24 -0700

    tsweb: remove JSONHandlerFunc
    It's unused and we've decided it's not what we want.
    
    Change-Id: I425a0104e8869630b498a0adfd0f455876d6f92b
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- tsweb/tsweb_test.go
+++ tsweb/tsweb_test.go
@@ -552,3 +552,31 @@ func (promWriter) WritePrometheus(w io.Writer, prefix string) {
 func (promWriter) String() string {
 	return ""
 }
+
+func TestAcceptsEncoding(t *testing.T) {
+	tests := []struct {
+		in, enc string
+		want    bool
+	}{
+		{"", "gzip", false},
+		{"gzip", "gzip", true},
+		{"foo,gzip", "gzip", true},
+		{"foo, gzip", "gzip", true},
+		{"foo, gzip ", "gzip", true},
+		{"gzip, foo ", "gzip", true},
+		{"gzip, foo ", "br", false},
+		{"gzip, foo ", "fo", false},
+		{"gzip;q=1.2, foo ", "gzip", true},
+		{" gzip;q=1.2, foo ", "gzip", true},
+	}
+	for i, tt := range tests {
+		h := make(http.Header)
+		if tt.in != "" {
+			h.Set("Accept-Encoding", tt.in)
+		}
+		got := AcceptsEncoding(&http.Request{Header: h}, tt.enc)
+		if got != tt.want {
+			t.Errorf("%d. got %v; want %v", i, got, tt.want)
+		}
+	}
+}
