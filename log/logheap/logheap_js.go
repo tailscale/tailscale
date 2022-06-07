c41837842baf3e26878f50255cf009b0a85702ca
Mihai Parparita <mihai@tailscale.com>
2022-06-07 11:50:12 -0700

    wasm: drop pprof dependency
    We can use the browser tools to profile, pprof adds 200K to the binary size.
    
    Updates #3157
    
    Signed-off-by: Mihai Parparita <mihai@tailscale.com>

---
+++ log/logheap/logheap_js.go
@@ -0,0 +1,8 @@
+// Copyright (c) 2022 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package logheap
+
+func LogHeap(postURL string) {
+}
