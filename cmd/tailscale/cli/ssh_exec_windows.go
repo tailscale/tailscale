71ee92fc5919a03e4474811a3f69fce587d3143f
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-06-25 21:17:42 +0900

    add comment
    Signed-off-by: Yasuhiro Matsumoto <mattn.jp@gmail.com>

--- cmd/tailscale/cli/ssh_exec_windows.go
+++ cmd/tailscale/cli/ssh_exec_windows.go
@@ -12,6 +12,8 @@ import (
 )
 
 func findSSH() (string, error) {
+	// use C:\Windows\System32\OpenSSH\ssh.exe since unexpected behavior
+	// occured with ssh.exe provided by msys2/cygwin and other environments.
 	if systemRoot := os.Getenv("SystemRoot"); systemRoot != "" {
 		exe := filepath.Join(systemRoot, "System32", "OpenSSH", "ssh.exe")
 		if st, err := os.Stat(exe); err == nil && !st.IsDir() {
