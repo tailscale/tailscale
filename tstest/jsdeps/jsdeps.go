de2dcda2e03c538e74260b6f8b5ad94314ced98c
Mihai Parparita <mihai@tailscale.com>
2022-07-19 14:32:06 -0700

    .github/workflows: update module that we try to build in cross-wasm
    We now have the actual module that we need to build, so switch to
    building it directly instead of its (expected) dependencies.
    
    Also fix a copy/paste error in a jsdeps comment.
    
    Signed-off-by: Mihai Parparita <mihai@tailscale.com>

--- tstest/jsdeps/jsdeps.go
+++ tstest/jsdeps/jsdeps.go
@@ -4,8 +4,8 @@
 
 // Package jsdeps is a just a list of the packages we import in the
 // JavaScript/WASM build, to let us test that our transitive closure of
-// dependencies on iOS doesn't accidentally grow too large, since binary size
-// is more of a concern there.
+// dependencies doesn't accidentally grow too large, since binary size
+// is more of a concern.
 package jsdeps
 
 import (
