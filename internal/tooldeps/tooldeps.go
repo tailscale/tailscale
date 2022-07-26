89928495d9ed29ccdfaa2db862505c2805626ac7
Brad Fitzpatrick <bradfitz@tailscale.com>
2022-07-26 09:20:53 -0700

    .github/workflows: add gofmt (goimports) check
    Change-Id: Iceb3182827b9c65f28f0351e0e254abe4a95e4de
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- internal/tooldeps/tooldeps.go
+++ internal/tooldeps/tooldeps.go
@@ -2,8 +2,12 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build for_go_mod_tidy_only
+// +build for_go_mod_tidy_only
+
 package tooldeps
 
 import (
 	_ "github.com/tailscale/depaware/depaware"
+	_ "golang.org/x/tools/cmd/goimports"
 )
