97b8c4fa1b8856294541051e7f3ed9eb3e1facc8
Brad Fitzpatrick <bradfitz@tailscale.com>
2022-07-27 14:08:59 -0700

    ipn/store/awsstore: add "ts_omit_aws" build tag to reduce binary size
    Drops tailscaled from 23M to 21M.
    
    Change-Id: I731c542d03113ac94abb695e3c8fcacbc5542712
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- ipn/store/awsstore/store_aws.go
+++ ipn/store/awsstore/store_aws.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build linux
-// +build linux
+//go:build linux && !ts_omit_aws
+// +build linux,!ts_omit_aws
 
 // Package awsstore contains an ipn.StateStore implementation using AWS SSM.
 package awsstore
