525999af6fa490d5764ed65643970a09034e9fbc
Aaron Klotz <aaron@tailscale.com>
2022-07-27 13:47:28 -0600

    net/tshttpproxy: fix incorrect type in Windows implementation, switch to mkwinsyscall, fix memory leak
    The definition of winHTTPProxyInfo was using the wrong type (uint16 vs uint32)
    for its first field. I fixed that type.
    
    Furthermore, any UTF16 strings returned in that structure must be explicitly
    freed. I added code to do this.
    
    Finally, since this is the second time I've seen type safety errors in this code,
    I switched the native API calls over to use wrappers generated by mkwinsyscall.
    I know that would not have helped prevent the previous two problems, but every
    bit helps IMHO.
    
    Updates https://github.com/tailscale/tailscale/issues/4811
    
    Signed-off-by: Aaron Klotz <aaron@tailscale.com>

---
+++ net/tshttpproxy/mksyscall.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package tshttpproxy
+
+//go:generate go run golang.org/x/sys/windows/mkwinsyscall -output zsyscall_windows.go mksyscall.go
+
+//sys globalFree(hglobal winHGlobal) (err error) [failretval==0] = kernel32.GlobalFree
+//sys winHTTPCloseHandle(whi winHTTPInternet) (err error) [failretval==0] = winhttp.WinHttpCloseHandle
+//sys winHTTPGetProxyForURL(whi winHTTPInternet, url *uint16, options *winHTTPAutoProxyOptions, proxyInfo *winHTTPProxyInfo) (err error) [failretval==0] = winhttp.WinHttpGetProxyForUrl
+//sys winHTTPOpen(agent *uint16, accessType uint32, proxy *uint16, proxyBypass *uint16, flags uint32) (whi winHTTPInternet, err error) [failretval==0] = winhttp.WinHttpOpen
