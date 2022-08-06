40ec8617ace095632aa2930aa51b09042c966d52
Maisem Ali <maisem@tailscale.com>
2022-08-05 22:39:17 -0700

    util/must: rename Do->Get, add Do
    Signed-off-by: Maisem Ali <maisem@tailscale.com>

--- util/must/must.go
+++ util/must/must.go
@@ -5,11 +5,20 @@
 // Package must assists in calling functions that must succeed.
 //
 // Example usage:
-//	var target = must.Do(url.Parse(...))
+//
+//	var target = must.Get(url.Parse(...))
+//	must.Do(close())
 package must
 
-// Do returns v as is. It panics if err is non-nil.
-func Do[T any](v T, err error) T {
+// Do panics if err is non-nil.
+func Do(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Get returns v as is. It panics if err is non-nil.
+func Get[T any](v T, err error) T {
 	if err != nil {
 		panic(err)
 	}
