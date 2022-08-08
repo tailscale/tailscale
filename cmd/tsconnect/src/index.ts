5d82b8dfa45edcc113c43fce679813154cb0f281
Charlotte Brandhorst-Satzkorn <charlotte@tailscale.com>
2022-08-08 15:22:49 -0700

    tsconnect: pass in authkey in dev mode
    This change allows for an auth key to be specified as a url query param
    for use in development mode. If an auth key is specified and valid, it
    will authorize the client for use immediately.
    
    Updates #5144
    
    Signed-off-by: Charlotte Brandhorst-Satzkorn <charlotte@tailscale.com>

--- cmd/tsconnect/src/index.ts
+++ cmd/tsconnect/src/index.ts
@@ -18,10 +18,18 @@ async function main() {
   go.run(wasmInstance.instance).then(() =>
     app.handleGoPanic("Unexpected shutdown")
   )
+
+  const params = new URLSearchParams(window.location.search)
+  const authKey = params.get("authkey") ?? undefined
+
   const ipn = newIPN({
     // Persist IPN state in sessionStorage in development, so that we don't need
     // to re-authorize every time we reload the page.
     stateStorage: DEBUG ? sessionStateStorage : undefined,
+    // authKey allows for an auth key to be
+    // specified as a url param which automatically
+    // authorizes the client for use.
+    authKey: DEBUG ? authKey : undefined,
   })
   app.runWithIPN(ipn)
 }
