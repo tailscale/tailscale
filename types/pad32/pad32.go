f4ff26f5774f2bc4c6ec1eb981739fb3218152ff
Brad Fitzpatrick <bradfitz@tailscale.com>
2022-10-30 20:15:30 -0700

    types/pad32: delete package
    Use Go 1.19's new 64-bit alignment ~hidden feature instead.
    
    Fixes #5356
    
    Change-Id: Ifcbcb115875a7da01df3bc29e9e7feadce5bc956
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- types/pad32/pad32.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2021 Tailscale Inc & AUTHORS All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Package pad32 defines padding types that have width on only 32-bit platforms.
-package pad32
-
-// Four is 4 bytes of padding on 32-bit machines, else 0 bytes.
-type Four [4 * (1 - ((^uint(0))>>32)&1)]byte
