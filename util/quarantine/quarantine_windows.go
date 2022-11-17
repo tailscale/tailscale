8cc1b08c29042f7a8429a5b16806fbc7d5879fd0
Andrew Dunham <andrew@du.nham.ca>
2022-11-17 12:42:02 -0500

    cmd/tailscale, util/quarantine: set quarantine flags on files from Taildrop
    This sets the "com.apple.quarantine" flag on macOS, and the
    "Zone.Identifier" alternate data stream on Windows.
    
    Change-Id: If14f805467b0e2963067937d7f34e08ba1d1fa85
    Signed-off-by: Andrew Dunham <andrew@du.nham.ca>
    (cherry picked from commit 0af61f7c401f88d5079ed06189ec5ad553d9fab3)

---
+++ util/quarantine/quarantine_windows.go
@@ -0,0 +1,30 @@
+// Copyright (c) 2022 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package quarantine
+
+import (
+	"os"
+	"strings"
+)
+
+func setQuarantineAttr(f *os.File) error {
+	// Documentation on this can be found here:
+	//    https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-fscc/6e3f7352-d11c-4d76-8c39-2516a9df36e8
+	//
+	// Additional information can be found at:
+	//    https://www.digital-detective.net/forensic-analysis-of-zone-identifier-stream/
+	//    https://bugzilla.mozilla.org/show_bug.cgi?id=1433179
+	content := strings.Join([]string{
+		"[ZoneTransfer]",
+
+		// "URLZONE_INTERNET"
+		// https://docs.microsoft.com/en-us/previous-versions/windows/internet-explorer/ie-developer/platform-apis/ms537175(v=vs.85)
+		"ZoneId=3",
+
+		// TODO(andrew): should/could we add ReferrerUrl or HostUrl?
+	}, "\r\n")
+
+	return os.WriteFile(f.Name()+":Zone.Identifier", []byte(content), 0)
+}
