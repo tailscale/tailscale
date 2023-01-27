56bdd1fca745e62c21c3ba94f540070d819883d0
Anton Tolchanov <anton@tailscale.com>
2023-01-27 14:49:50 +0000

    cmd/derpprobe: migrate to the prober framework
    `prober.DERP` was created in #5988 based on derpprobe. Having used it
    instead of derpprobe for a few months, I think we have enough confidence
    that it works and can now migrate derpprobe to use the prober framework
    and get rid of code duplication.
    
    A few notable changes in behaviour:
    - results of STUN probes over IPv4 and IPv6 are now reported separately;
    - TLS probing now includes OCSP verification;
    - probe names in the output have changed;
    - ability to send Slack notification from the prober has been removed.
      Instead, the prober now exports metrics in Expvar (/debug/vars) and
      Prometheus (/debug/varz) formats.
    
    Fixes https://github.com/tailscale/corp/issues/8497
    
    Signed-off-by: Anton Tolchanov <anton@tailscale.com>

--- prober/derp.go
+++ prober/derp.go
@@ -158,7 +158,7 @@ func (d *derpProber) updateMap(ctx context.Context) error {
 	if err != nil {
 		return nil
 	}
-	res, err := http.DefaultClient.Do(req)
+	res, err := httpOrFileClient.Do(req)
 	if err != nil {
 		d.Lock()
 		defer d.Unlock()
@@ -389,4 +389,12 @@ func newConn(ctx context.Context, dm *tailcfg.DERPMap, n *tailcfg.DERPNode) (*de
 		return nil, fmt.Errorf("timeout waiting for ServerInfoMessage: %w", ctx.Err())
 	}
 	return dc, nil
+}
+
+var httpOrFileClient = &http.Client{Transport: httpOrFileTransport()}
+
+func httpOrFileTransport() http.RoundTripper {
+	tr := http.DefaultTransport.(*http.Transport).Clone()
+	tr.RegisterProtocol("file", http.NewFileTransport(http.Dir("/")))
+	return tr
 }
