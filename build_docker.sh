f991288ceb3047c65aa96581ea04d5ca68dc7b36
David Anderson <danderson@tailscale.com>
2023-01-27 16:32:32 -0800

    build_docker.sh: don't push to ghcr by default.
    Signed-off-by: David Anderson <danderson@tailscale.com>

--- build_docker.sh
+++ build_docker.sh
@@ -24,7 +24,7 @@ export PATH=$PWD/tool:$PATH
 
 eval $(./build_dist.sh shellvars)
 DEFAULT_TAGS="v${VERSION_SHORT},v${VERSION_MINOR}"
-DEFAULT_REPOS="tailscale/tailscale,ghcr.io/tailscale/tailscale"
+DEFAULT_REPOS="tailscale/tailscale"
 DEFAULT_BASE="ghcr.io/tailscale/alpine-base:3.16"
 DEFAULT_TARGET="client"
 
