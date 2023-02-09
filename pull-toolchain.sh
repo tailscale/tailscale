395919ad62286edfffea604aae4cfcfeb1ba9e45
David Anderson <danderson@tailscale.com>
2023-02-09 15:00:05 -0800

    pull-toolchain.sh: don't run update-flake.sh
    We no longer carry an SRI hash for the toolchain, so flake
    updating is no longer needed for toolchain changes.
    
    Signed-off-by: David Anderson <danderson@tailscale.com>

--- pull-toolchain.sh
+++ pull-toolchain.sh
@@ -9,9 +9,8 @@ upstream=$(git ls-remote https://github.com/tailscale/go "$go_branch" | awk '{pr
 current=$(cat go.toolchain.rev)
 if [ "$upstream" != "$current" ]; then
 	echo "$upstream" >go.toolchain.rev
-	./update-flake.sh
 fi
 
-if [ -n "$(git diff-index --name-only HEAD -- go.toolchain.rev go.mod.sri)" ]; then
+if [ -n "$(git diff-index --name-only HEAD -- go.toolchain.rev)" ]; then
     echo "pull-toolchain.sh: changes imported. Use git commit to make them permanent." >&2
 fi
