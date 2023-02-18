db8d5bba5ed690738e97eed8eaab4406ee12cffe
David Anderson <danderson@tailscale.com>
2023-02-17 20:37:42 -0800

    version/mkversion: add documentation, rename internal terminology
    Signed-off-by: David Anderson <danderson@tailscale.com>

--- version/mkversion/mkversion_test.go
+++ version/mkversion/mkversion_test.go
@@ -12,28 +12,26 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func mkInfo(gitHash, otherHash, corpDate string, major, minor, patch, changeCount int) verInfo {
+func mkInfo(gitHash, otherHash, otherDate string, major, minor, patch, changeCount int) verInfo {
 	return verInfo{
-		ossInfo: ossInfo{
-			major:       major,
-			minor:       minor,
-			patch:       patch,
-			changeCount: changeCount,
-			hash:        gitHash,
-		},
-		corpHash: otherHash,
-		corpDate: corpDate,
+		major:       major,
+		minor:       minor,
+		patch:       patch,
+		changeCount: changeCount,
+		hash:        gitHash,
+		otherHash:   otherHash,
+		otherDate:   otherDate,
 	}
 }
 
 func TestMkversion(t *testing.T) {
-	corpDate := fmt.Sprintf("%d", time.Date(2023, time.January, 27, 1, 2, 3, 4, time.UTC).Unix())
+	otherDate := fmt.Sprintf("%d", time.Date(2023, time.January, 27, 1, 2, 3, 4, time.UTC).Unix())
 
 	tests := []struct {
 		in   verInfo
 		want string
 	}{
-		{mkInfo("abcdef", "", corpDate, 0, 98, 0, 0), `
+		{mkInfo("abcdef", "", otherDate, 0, 98, 0, 0), `
            VERSION_SHORT="0.98.0"
            VERSION_LONG="0.98.0-tabcdef"
            VERSION_GIT_HASH="abcdef"
@@ -45,7 +43,7 @@ func TestMkversion(t *testing.T) {
            VERSION_MSIPRODUCT_AMD64="C653B075-AD91-5265-9DF8-0087D35D148D"
            VERSION_MSIPRODUCT_ARM64="1C41380B-A742-5A3C-AF5D-DF7894DD0FB8"
            VERSION_MSIPRODUCT_X86="4ABDDA14-7499-5C2E-A62A-DD435C50C4CB"`},
-		{mkInfo("abcdef", "", corpDate, 0, 98, 1, 0), `
+		{mkInfo("abcdef", "", otherDate, 0, 98, 1, 0), `
            VERSION_SHORT="0.98.1"
            VERSION_LONG="0.98.1-tabcdef"
            VERSION_GIT_HASH="abcdef"
@@ -57,7 +55,7 @@ func TestMkversion(t *testing.T) {
            VERSION_MSIPRODUCT_AMD64="DFD6DCF2-06D8-5D19-BDA0-FAF31E44EC23"
            VERSION_MSIPRODUCT_ARM64="A4CCF19C-372B-5007-AFD8-1AF661DFF670"
            VERSION_MSIPRODUCT_X86="FF12E937-DDC4-5868-9B63-D35B2050D4EA"`},
-		{mkInfo("abcdef", "", corpDate, 1, 2, 9, 0), `
+		{mkInfo("abcdef", "", otherDate, 1, 2, 9, 0), `
            VERSION_SHORT="1.2.9"
            VERSION_LONG="1.2.9-tabcdef"
            VERSION_GIT_HASH="abcdef"
@@ -69,7 +67,7 @@ func TestMkversion(t *testing.T) {
            VERSION_MSIPRODUCT_AMD64="D47B5157-FF26-5A10-A94E-50E4529303A9"
            VERSION_MSIPRODUCT_ARM64="91D16F75-2A12-5E12-820A-67B89BF858E7"
            VERSION_MSIPRODUCT_X86="8F1AC1C6-B93B-5C70-802E-6AE9591FA0D6"`},
-		{mkInfo("abcdef", "", corpDate, 1, 15, 0, 129), `
+		{mkInfo("abcdef", "", otherDate, 1, 15, 0, 129), `
            VERSION_SHORT="1.15.129"
            VERSION_LONG="1.15.129-tabcdef"
            VERSION_GIT_HASH="abcdef"
@@ -81,7 +79,7 @@ func TestMkversion(t *testing.T) {
            VERSION_MSIPRODUCT_AMD64="89C96952-1FB8-5A4D-B02E-16A8060C56AA"
            VERSION_MSIPRODUCT_ARM64="DB1A2E86-66C4-5CEC-8F4C-7DB805370F3A"
            VERSION_MSIPRODUCT_X86="DC57C0C3-5164-5C92-86B3-2800CEFF0540"`},
-		{mkInfo("abcdef", "", corpDate, 1, 2, 0, 17), `
+		{mkInfo("abcdef", "", otherDate, 1, 2, 0, 17), `
            VERSION_SHORT="1.2.0"
            VERSION_LONG="1.2.0-17-tabcdef"
            VERSION_GIT_HASH="abcdef"
@@ -93,7 +91,7 @@ func TestMkversion(t *testing.T) {
            VERSION_MSIPRODUCT_AMD64="0F9709AE-0E5E-51AF-BCCD-A25314B4CE8B"
            VERSION_MSIPRODUCT_ARM64="39D5D46E-E644-5C80-9EF8-224AC1AD5969"
            VERSION_MSIPRODUCT_X86="4487136B-2D11-5E42-BD80-B8529F3326F4"`},
-		{mkInfo("abcdef", "defghi", corpDate, 1, 15, 0, 129), `
+		{mkInfo("abcdef", "defghi", otherDate, 1, 15, 0, 129), `
            VERSION_SHORT="1.15.129"
            VERSION_LONG="1.15.129-tabcdef-gdefghi"
            VERSION_GIT_HASH="abcdef"
@@ -105,7 +103,7 @@ func TestMkversion(t *testing.T) {
            VERSION_MSIPRODUCT_AMD64="89C96952-1FB8-5A4D-B02E-16A8060C56AA"
            VERSION_MSIPRODUCT_ARM64="DB1A2E86-66C4-5CEC-8F4C-7DB805370F3A"
            VERSION_MSIPRODUCT_X86="DC57C0C3-5164-5C92-86B3-2800CEFF0540"`},
-		{mkInfo("abcdef", "", corpDate, 1, 2, 0, 17), `
+		{mkInfo("abcdef", "", otherDate, 1, 2, 0, 17), `
            VERSION_SHORT="1.2.0"
            VERSION_LONG="1.2.0-17-tabcdef"
            VERSION_GIT_HASH="abcdef"
@@ -117,7 +115,7 @@ func TestMkversion(t *testing.T) {
            VERSION_MSIPRODUCT_AMD64="0F9709AE-0E5E-51AF-BCCD-A25314B4CE8B"
            VERSION_MSIPRODUCT_ARM64="39D5D46E-E644-5C80-9EF8-224AC1AD5969"
            VERSION_MSIPRODUCT_X86="4487136B-2D11-5E42-BD80-B8529F3326F4"`},
-		{mkInfo("abcdef", "defghi", corpDate, 1, 15, 0, 129), `
+		{mkInfo("abcdef", "defghi", otherDate, 1, 15, 0, 129), `
            VERSION_SHORT="1.15.129"
            VERSION_LONG="1.15.129-tabcdef-gdefghi"
            VERSION_GIT_HASH="abcdef"
@@ -129,9 +127,9 @@ func TestMkversion(t *testing.T) {
            VERSION_MSIPRODUCT_AMD64="89C96952-1FB8-5A4D-B02E-16A8060C56AA"
            VERSION_MSIPRODUCT_ARM64="DB1A2E86-66C4-5CEC-8F4C-7DB805370F3A"
            VERSION_MSIPRODUCT_X86="DC57C0C3-5164-5C92-86B3-2800CEFF0540"`},
-		{mkInfo("abcdef", "", corpDate, 0, 99, 5, 0), ""},   // unstable, patch number not allowed
-		{mkInfo("abcdef", "", corpDate, 0, 99, 5, 123), ""}, // unstable, patch number not allowed
-		{mkInfo("abcdef", "defghi", "", 1, 15, 0, 129), ""}, // missing corpDate
+		{mkInfo("abcdef", "", otherDate, 0, 99, 5, 0), ""},   // unstable, patch number not allowed
+		{mkInfo("abcdef", "", otherDate, 0, 99, 5, 123), ""}, // unstable, patch number not allowed
+		{mkInfo("abcdef", "defghi", "", 1, 15, 0, 129), ""},  // missing otherDate
 	}
 
 	for _, test := range tests {
