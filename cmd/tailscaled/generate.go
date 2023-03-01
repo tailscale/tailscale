f18beaa1e4e31aa42f3125de5be06da057e5340f
Aaron Klotz <aaron@tailscale.com>
2023-03-01 15:36:21 -0700

    cmd/mkmanifest, cmd/tailscale, cmd/tailscaled: remove Windows arm32 resources from OSS
    Given recent changes in corp, I originally thought we could remove all of the
    syso files, but then I realized that we still need them so that binaries built
    purely from OSS (without going through corp) will still receive a manifest.
    
    We can remove the arm32 one though, since we don't support 32-bit ARM on Windows.
    
    Updates https://github.com/tailscale/corp/issues/9576
    
    Signed-off-by: Aaron Klotz <aaron@tailscale.com>

--- cmd/tailscaled/generate.go
+++ cmd/tailscaled/generate.go
@@ -6,4 +6,3 @@ package main
 //go:generate go run tailscale.com/cmd/mkmanifest amd64 windows-manifest.xml manifest_windows_amd64.syso
 //go:generate go run tailscale.com/cmd/mkmanifest 386 windows-manifest.xml manifest_windows_386.syso
 //go:generate go run tailscale.com/cmd/mkmanifest arm64 windows-manifest.xml manifest_windows_arm64.syso
-//go:generate go run tailscale.com/cmd/mkmanifest arm windows-manifest.xml manifest_windows_arm.syso
