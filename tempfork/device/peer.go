5946392e6e03b4234d7ad0b09bcb87a915a085a4
Maisem Ali <maisem@tailscale.com>
2023-03-29 09:49:33 -0700

    net/tstun/table: add initial RoutingTable implementation
    It is based on `*tempfork/device.AllowedIPs`.
    
    Updates tailscale/corp#8020
    
    Signed-off-by: Maisem Ali <maisem@tailscale.com>

--- tempfork/device/peer.go
+++ tempfork/device/peer.go
@@ -7,8 +7,22 @@ package device
 
 import (
 	"container/list"
+
+	"tailscale.com/types/key"
 )
 
 type Peer struct {
 	trieEntries list.List
+
+	key key.NodePublic
+}
+
+func NewPeer(k key.NodePublic) *Peer {
+	return &Peer{
+		key: k,
+	}
+}
+
+func (p *Peer) Key() key.NodePublic {
+	return p.key
 }
