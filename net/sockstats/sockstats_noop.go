782ccb56550164cb722b3c5cf6d0780bb94826d8
Mihai Parparita <mihai@tailscale.com>
2023-04-12 15:50:52 -0700

    .github/workflows: run one set of tests with the tailscale_go build tag
    We use it to gate code that depends on custom Go toolchain, but it's
    currently only passed in the corp runners. Add a set on OSS so that we
    can catch regressions earlier.
    
    To specifically test sockstats this required adding a build tag to
    explicitly enable them -- they're normally on for iOS, macOS and Android
    only, and we don't run tests on those platforms normally.
    
    Signed-off-by: Mihai Parparita <mihai@tailscale.com>

--- net/sockstats/sockstats_noop.go
+++ net/sockstats/sockstats_noop.go
@@ -1,7 +1,7 @@
 // Copyright (c) Tailscale Inc & AUTHORS
 // SPDX-License-Identifier: BSD-3-Clause
 
-//go:build !tailscale_go || !(darwin || ios || android)
+//go:build !tailscale_go || !(darwin || ios || android || ts_enable_sockstats)
 
 package sockstats
 
