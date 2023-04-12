9e50da321b96567e688b68b7a1d9510c20642766
Brad Fitzpatrick <bradfitz@tailscale.com>
2023-04-12 10:24:17 -0700

    client/tailscale: dial LocalAPI at 127.0.0.1 on macOS
    Updates #7851
    
    Change-Id: Ib53cf53cdfee277ef42f7833352bc51ecb5db959
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- client/tailscale/localclient.go
+++ client/tailscale/localclient.go
@@ -96,8 +96,9 @@ func (lc *LocalClient) defaultDialer(ctx context.Context, network, addr string) 
 		// a TCP server on a random port, find the random port. For HTTP connections,
 		// we don't send the token. It gets added in an HTTP Basic-Auth header.
 		if port, _, err := safesocket.LocalTCPPortAndToken(); err == nil {
+			// We use 127.0.0.1 and not "localhost" (issue 7851).
 			var d net.Dialer
-			return d.DialContext(ctx, "tcp", "localhost:"+strconv.Itoa(port))
+			return d.DialContext(ctx, "tcp", "127.0.0.1:"+strconv.Itoa(port))
 		}
 	}
 	s := safesocket.DefaultConnectionStrategy(lc.socket())
