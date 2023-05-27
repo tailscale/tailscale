d7edead8cfe9fa519c5337f16f3c9a90c92dabe0
Heiko Rothe <me@heikorothe.com>
2023-05-27 22:56:31 +0200

    ipn/ipnlocal: [serve/funnel] add forwarded host and proto header
    This replicates the headers also sent by the golang reverse proxy by
    default.
    
    Fixes #7061
    
    Signed-off-by: Heiko Rothe <me@heikorothe.com>

--- ipn/ipnlocal/serve.go
+++ ipn/ipnlocal/serve.go
@@ -447,8 +447,14 @@ func (b *LocalBackend) proxyHandlerForBackend(backend string) (*httputil.Reverse
 		Rewrite: func(r *httputil.ProxyRequest) {
 			r.SetURL(u)
 			r.Out.Host = r.In.Host
+			r.Out.Header.Set("X-Forwarded-Host", r.In.Host)
 			if c, ok := r.Out.Context().Value(serveHTTPContextKey{}).(*serveHTTPContext); ok {
 				r.Out.Header.Set("X-Forwarded-For", c.SrcAddr.Addr().String())
+			}
+			if r.In.TLS == nil {
+				r.Out.Header.Set("X-Forwarded-Proto", "http")
+			} else {
+				r.Out.Header.Set("X-Forwarded-Proto", "https")
 			}
 		},
 		Transport: &http.Transport{
