b48fe11964a3b0c564192692595f0db953bd9384
Xe Iaso <xe@tailscale.com>
2023-06-06 18:00:53 +0000

    tsnet: document how to use Dir with multiple servers per process
    Followup from a conversation on the Gophers slack, also matches the
    examples in the tsnet kb.
    
    Closes #8287
    
    Signed-off-by: Xe Iaso <xe@tailscale.com>

--- tsnet/tsnet.go
+++ tsnet/tsnet.go
@@ -66,6 +66,12 @@ type Server struct {
 	// state. If empty, a directory is selected automatically
 	// under os.UserConfigDir (https://golang.org/pkg/os/#UserConfigDir).
 	// based on the name of the binary.
+	//
+	// If you want to use multiple tsnet services in the same
+	// binary, you will need to make sure that Dir is set uniquely
+	// for each service. A good pattern for this is to have a
+	// "base" directory (such as your mutable storage folder) and
+	// then append the hostname on the end of it.
 	Dir string
 
 	// Store specifies the state store to use.
