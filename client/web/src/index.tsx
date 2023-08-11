538e23e33f3e8d43fe14342ec9915ad375886744
Sonia Appasamy <sonia@tailscale.com>
2023-08-11 10:53:27 -0400

    client/web: pipe through to React in dev mode
    Updates tailscale/corp#13775
    
    Signed-off-by: Sonia Appasamy <sonia@tailscale.com>
    Signed-off-by: Alex Paguis <alex@windscribe.com>

---
+++ client/web/src/index.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { createRoot } from "react-dom/client"
+import App from "./components/app"
+
+const rootEl = document.createElement("div")
+rootEl.id = "app-root"
+rootEl.classList.add("relative", "z-0")
+document.body.append(rootEl)
+
+const root = createRoot(rootEl)
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
