75248256769e0d32410b8b274b154d1e21191457
Sonia Appasamy <sonia@tailscale.com>
2023-08-11 11:44:57 -0400

    client/web: add tsconfig.json
    Also allows us to use absolute import paths (see change in index.tsx).
    
    Updates tailscale/corp#13775
    
    Signed-off-by: Sonia Appasamy <sonia@tailscale.com>

--- client/web/src/index.tsx
+++ client/web/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-import App from "./components/app"
+import App from "src/components/app"
 
 const rootEl = document.createElement("div")
 rootEl.id = "app-root"
