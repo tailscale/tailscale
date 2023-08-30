58040cabf9c3c2099886f9823ddcd368f8cc4cfe
Andrea Barisani <andrea@inversepath.com>
2023-08-30 14:23:38 +0200

    adjust build tags for tamago
    Signed-off-by: Andrea Barisani <andrea@inversepath.com>
    Signed-off-by: Alex Paguis <alex@windscribe.com>

--- net/tstun/tun.go
+++ net/tstun/tun.go
@@ -1,7 +1,7 @@
 // Copyright (c) Tailscale Inc & AUTHORS
 // SPDX-License-Identifier: BSD-3-Clause
 
-//go:build !wasm && !plan9
+//go:build !wasm && !plan9 && !tamago
 
 // Package tun creates a tuntap device, working around OS-specific
 // quirks if necessary.
