d2ea9bb1eb020f635906ec6059faaf6403b854f5
Brad Fitzpatrick <bradfitz@tailscale.com>
2023-09-30 10:09:56 -0700

    cmd/cloner: fix typo in test type's name
    s/SliceContianer/SliceContainer/g
    
    Updates #9604
    
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- cmd/cloner/clonerex/clonerex.go
+++ cmd/cloner/clonerex/clonerex.go
@@ -1,10 +1,10 @@
 // Copyright (c) Tailscale Inc & AUTHORS
 // SPDX-License-Identifier: BSD-3-Clause
 
-//go:generate go run tailscale.com/cmd/cloner  -clonefunc=true -type SliceContianer
+//go:generate go run tailscale.com/cmd/cloner  -clonefunc=true -type SliceContainer
 
 package clonerex
 
-type SliceContianer struct {
+type SliceContainer struct {
 	Slice []*int
 }
