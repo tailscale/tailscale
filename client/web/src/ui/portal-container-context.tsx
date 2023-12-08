78b8e7052faf3ff5a6af0e207173a5ef517aff8e
Sonia Appasamy <sonia@tailscale.com>
2023-12-08 18:55:57 -0500

    client/web: add confirmation dialogs
    Add confirmation dialogs for disconnecting and stopping advertisement
    of a subnet route.
    
    Updates #10261
    
    Signed-off-by: Sonia Appasamy <sonia@tailscale.com>

---
+++ client/web/src/ui/portal-container-context.tsx
@@ -0,0 +1,9 @@
+// Copyright (c) Tailscale Inc & AUTHORS
+// SPDX-License-Identifier: BSD-3-Clause
+
+import React from "react"
+
+const PortalContainerContext = React.createContext<HTMLElement | undefined>(
+  undefined
+)
+export default PortalContainerContext
