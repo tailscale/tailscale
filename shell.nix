e00141ccbed1bf39a539e01d670d0e3e61f78ac4
Flakes Updater <noreply+flakes-updater@tailscale.com>
2023-12-13 17:43:27 +0000

    go.mod.sri: update SRI hash for go.mod changes
    Signed-off-by: Flakes Updater <noreply+flakes-updater@tailscale.com>

--- shell.nix
+++ shell.nix
@@ -16,4 +16,4 @@
 ) {
   src =  ./.;
 }).shellNix
-# nix-direnv cache busting line: sha256-VbRVOzEHj80mDOnQyIh/jds5NNzqc/1Ft0A7SegD6wA=
+# nix-direnv cache busting line: sha256-bG/ydsJf2UncOcDo8/BXdvQJO3Mk0tl8JGje1b6kto4=
