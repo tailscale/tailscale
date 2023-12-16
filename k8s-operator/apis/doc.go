577df98843511d67e35033d6681232dc7735ee74
Irbe Krumina <irbe@tailscale.com>
2023-12-16 09:15:40 +0000

    k8s-operator: remove build tags that exclude plan9
    This library is now only compiled into ./cmd/k8s-operator so we should no longer need worry about it failing on plan9
    
    Updates #cleanup
    
    Signed-off-by: Irbe Krumina <irbe@tailscale.com>

--- k8s-operator/apis/doc.go
+++ k8s-operator/apis/doc.go
@@ -1,8 +1,6 @@
 // Copyright (c) Tailscale Inc & AUTHORS
 // SPDX-License-Identifier: BSD-3-Clause
 
-//go:build !plan9
-
 package apis
 
 const GroupName = "tailscale.com"
--- k8s-operator/apis/v1alpha1/doc.go
+++ k8s-operator/apis/v1alpha1/doc.go
@@ -1,8 +1,6 @@
 // Copyright (c) Tailscale Inc & AUTHORS
 // SPDX-License-Identifier: BSD-3-Clause
 
-//go:build !plan9
-
 // +kubebuilder:object:generate=true
 // +groupName=tailscale.com
 package v1alpha1
