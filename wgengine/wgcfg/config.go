d7bcee924e6ef53480428eda01b440fbd79f4fd5
Brad Fitzpatrick <bradfitz@tailscale.com>
2024-02-24 14:12:03 -0800

    wgengine/wgcfg: don't send UAPI to disable keep-alives on new peers
    That's already the default. Avoid the overhead of writing it on one
    side and reading it on the other to do nothing.
    
    Updates #cleanup (noticed while researching something else)
    
    Change-Id: I449c88a022271afb9be5da876bfaf438fe5d3f58
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- wgengine/wgcfg/config.go
+++ wgengine/wgcfg/config.go
@@ -39,7 +39,7 @@ type Peer struct {
 	AllowedIPs          []netip.Prefix
 	V4MasqAddr          *netip.Addr // if non-nil, masquerade IPv4 traffic to this peer using this address
 	V6MasqAddr          *netip.Addr // if non-nil, masquerade IPv6 traffic to this peer using this address
-	PersistentKeepalive uint16
+	PersistentKeepalive uint16      // in seconds between keep-alives; 0 to disable
 	// wireguard-go's endpoint for this peer. It should always equal Peer.PublicKey.
 	// We represent it explicitly so that we can detect if they diverge and recover.
 	// There is no need to set WGEndpoint explicitly when constructing a Peer by hand.
