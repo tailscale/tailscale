6a860cfb3514e8d423ef1fac4b7205a4f53ef394
Brad Fitzpatrick <bradfitz@tailscale.com>
2024-03-20 21:02:33 -0700

    ipn/ipnlocal: add c2n pprof option to force a GC
    Like net/http/pprof has.
    
    Updates tailscale/corp#18514
    
    Change-Id: I264adb6dcf5732d19707783b29b7273b4ca69cf4
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- ipn/ipnlocal/c2n_pprof.go
+++ ipn/ipnlocal/c2n_pprof.go
@@ -7,11 +7,17 @@ package ipnlocal
 
 import (
 	"net/http"
+	"runtime"
 	"runtime/pprof"
+	"strconv"
 )
 
 func init() {
 	c2nLogHeap = func(w http.ResponseWriter, r *http.Request) {
+		// Support same optional gc parameter as net/http/pprof:
+		if gc, _ := strconv.Atoi(r.FormValue("gc")); gc > 0 {
+			runtime.GC()
+		}
 		pprof.WriteHeapProfile(w)
 	}
 }
