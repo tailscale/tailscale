2cc769b2fdf770cdaade901e31a044ccc41f3fce
Sonia Appasamy <sonia@tailscale.com>
2024-04-24 15:02:27 -0400

    release/dist/qnap: omit .qpkg.codesigning files
    Updates tailscale/tailscale-qpkg#135
    
    Signed-off-by: Sonia Appasamy <sonia@tailscale.com>

--- release/dist/qnap/files/scripts/build-qpkg.sh
+++ release/dist/qnap/files/scripts/build-qpkg.sh
@@ -7,6 +7,14 @@ function cleanup() {
     rm -rf /Tailscale/$ARCH
     rm -f /Tailscale/sed*
     rm -f /Tailscale/qpkg.cfg
+
+    # If this build was signed, a .qpkg.codesigning file will be created as an
+    # artifact of the build
+    # (see https://github.com/qnap-dev/qdk2/blob/93ac75c76941b90ee668557f7ce01e4b23881054/QDK_2.x/bin/qbuild#L992).
+    #
+    # go/client-release doesn't seem to need these, so we delete them here to
+    # avoid uploading them to pkgs.tailscale.com.
+    rm -f /out/*.qpkg.codesigning
 }
 trap cleanup EXIT
 
