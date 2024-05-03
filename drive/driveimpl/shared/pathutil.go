7209c4f91e97eda632f86c9f0def600cf0a03949
Percy Wegmann <percy@tailscale.com>
2024-05-03 17:20:13 -0500

    drive: parse depth 1 PROPFIND results to include children in cache
    Clients often perform a PROPFIND for the parent directory before
    performing PROPFIND for specific children within that directory.
    The PROPFIND for the parent directory is usually done at depth 1,
    meaning that we already have information for all of the children.
    By immediately adding that to the cache, we save a roundtrip to
    the remote peer on the PROPFIND for the specific child.
    
    Updates tailscale/corp#19779
    
    Signed-off-by: Percy Wegmann <percy@tailscale.com>

--- drive/driveimpl/shared/pathutil.go
+++ drive/driveimpl/shared/pathutil.go
@@ -26,6 +26,17 @@ func CleanAndSplit(p string) []string {
 	return strings.Split(strings.Trim(path.Clean(p), sepStringAndDot), sepString)
 }
 
+// Normalize normalizes the given path (e.g. dropping trailing slashes).
+func Normalize(p string) string {
+	return Join(CleanAndSplit(p)...)
+}
+
+// Parent extracts the parent of the given path.
+func Parent(p string) string {
+	parts := CleanAndSplit(p)
+	return Join(parts[:len(parts)-1]...)
+}
+
 // Join behaves like path.Join() but also includes a leading slash.
 func Join(parts ...string) string {
 	fullParts := make([]string, 0, len(parts))
