4b7c80b71f36721bc8758fa2e99f17ef87c1af12
Brad Fitzpatrick <bradfitz@tailscale.com>
2024-05-29 22:00:56 -0700

    cmd/tailscaled, ipn/conffile: support ec2 user-data config file
    Updates #1412
    Updates #1866
    
    Change-Id: I4d08fb233b80c2078b3b28ffc18559baabb4a081
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

---
+++ omit/aws_omit.go
@@ -0,0 +1,9 @@
+// Copyright (c) Tailscale Inc & AUTHORS
+// SPDX-License-Identifier: BSD-3-Clause
+
+//go:build ts_omit_aws
+
+package omit
+
+// AWS is whether AWS support should be omitted from the build.
+const AWS = true
