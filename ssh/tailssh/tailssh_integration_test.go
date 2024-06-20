7383e642c38aef420c75bfbb494777a25553ad2f
Irbe Krumina <irbe@tailscale.com>
2024-06-20 19:14:36 +0100

    ssh/tailssh: fix integration test
    Updates#cleanup
    
    Signed-off-by: Irbe Krumina <irbe@tailscale.com>

--- ssh/tailssh/tailssh_integration_test.go
+++ ssh/tailssh/tailssh_integration_test.go
@@ -611,7 +611,7 @@ func (tb *testBackend) NetMap() *netmap.NetworkMap {
 	}
 }
 
-func (tb *testBackend) WhoIs(ipp netip.AddrPort) (n tailcfg.NodeView, u tailcfg.UserProfile, ok bool) {
+func (tb *testBackend) WhoIs(_ string, ipp netip.AddrPort) (n tailcfg.NodeView, u tailcfg.UserProfile, ok bool) {
 	return (&tailcfg.Node{}).View(), tailcfg.UserProfile{
 		LoginName: tb.localUser + "@example.com",
 	}, true
