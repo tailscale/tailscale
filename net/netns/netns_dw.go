4e761c7734287609f7115db124b48e48c622298a
Aaron Klotz <aaron@tailscale.com>
2024-07-31 11:45:14 -0600

    net/netns: remove some logspam by avoiding logging parse errors due to unspecified addresses
    I updated the address parsing stuff to return a specific error for
    unspecified hosts passed as empty strings, and look for that
    when logging errors. I explicitly did not make parseAddress return a
    netip.Addr containing an unspecified address because at this layer,
    in the absence of any host, we don't necessarily know the address
    family we're dealing with.
    
    For the purposes of this code I think this is fine, at least until
    we implement #12588.
    
    Fixes #12979
    
    Signed-off-by: Aaron Klotz <aaron@tailscale.com>

--- net/netns/netns_dw.go
+++ net/netns/netns_dw.go
@@ -6,15 +6,21 @@
 package netns
 
 import (
+	"errors"
 	"net"
 	"net/netip"
 )
+
+var errUnspecifiedHost = errors.New("unspecified host")
 
 func parseAddress(address string) (addr netip.Addr, err error) {
 	host, _, err := net.SplitHostPort(address)
 	if err != nil {
 		// error means the string didn't contain a port number, so use the string directly
 		host = address
+	}
+	if host == "" {
+		return addr, errUnspecifiedHost
 	}
 
 	return netip.ParseAddr(host)
