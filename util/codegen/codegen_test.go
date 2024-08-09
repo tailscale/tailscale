2506bf5b0699f29f101b1323930cdd6280575984
cai.li <cai.li@qingteng.cn>
2024-08-09 21:09:17 +0800

    fix #13076: codegen error when using anonymous struct
    Signed-off-by: cai.li <cai.li@qingteng.cn>

--- util/codegen/codegen_test.go
+++ util/codegen/codegen_test.go
@@ -4,8 +4,10 @@
 package codegen
 
 import (
+	"go/types"
 	"log"
 	"net/netip"
+	"strings"
 	"testing"
 	"unsafe"
 
@@ -170,6 +172,82 @@ func TestGenericContainsPointers(t *testing.T) {
 			typ := namedTypes[tt.typ]
 			if isPointer := ContainsPointers(typ); isPointer != tt.wantPointer {
 				t.Fatalf("ContainsPointers: got %v, want: %v", isPointer, tt.wantPointer)
+			}
+		})
+	}
+}
+
+func TestAssertStructUnchanged(t *testing.T) {
+	type args struct {
+		t      *types.Struct
+		tname  string
+		params *types.TypeParamList
+		ctx    string
+		it     *ImportTracker
+	}
+
+	// package t1 with a struct T1 with two fields
+	p1 := types.NewPackage("t1", "t1")
+	t1 := types.NewNamed(types.NewTypeName(0, p1, "T1", nil), types.NewStruct([]*types.Var{
+		types.NewField(0, nil, "P1", types.Typ[types.Int], false),
+		types.NewField(0, nil, "P2", types.Typ[types.String], false),
+	}, nil), nil)
+	p1.Scope().Insert(t1.Obj())
+
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		{
+			name: "t1-internally_defined",
+			args: args{
+				t:      t1.Underlying().(*types.Struct),
+				tname:  "prefix_",
+				params: nil,
+				ctx:    "",
+				it:     NewImportTracker(p1),
+			},
+			want: []byte("var _prefix_NeedsRegeneration = prefix_(struct {\n\tP1 int \n\tP2 string \n}{})"),
+		},
+		{
+			name: "t2-with_named_field",
+			args: args{
+				t: types.NewStruct([]*types.Var{
+					types.NewField(0, nil, "T1", t1, false),
+					types.NewField(0, nil, "P1", types.Typ[types.Int], false),
+					types.NewField(0, nil, "P2", types.Typ[types.String], false),
+				}, nil),
+				tname:  "prefix_",
+				params: nil,
+				ctx:    "",
+				it:     NewImportTracker(types.NewPackage("t2", "t2")),
+			},
+			// the struct should be regenerated with the named field
+			want: []byte("var _prefix_NeedsRegeneration = prefix_(struct {\n\tT1 t1.T1 \n\tP1 int \n\tP2 string \n}{})"),
+		},
+		{
+			name: "t3-with_embedded_field",
+			args: args{
+				t: types.NewStruct([]*types.Var{
+					types.NewField(0, nil, "T1", t1, true),
+					types.NewField(0, nil, "P1", types.Typ[types.Int], false),
+					types.NewField(0, nil, "P2", types.Typ[types.String], false),
+				}, nil),
+				tname:  "prefix_",
+				params: nil,
+				ctx:    "",
+				it:     NewImportTracker(types.NewPackage("t3", "t3")),
+			},
+			// the struct should be regenerated with the embedded field
+			want: []byte("var _prefix_NeedsRegeneration = prefix_(struct {\n\tt1.T1 \n\tP1 int \n\tP2 string \n}{})"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := AssertStructUnchanged(tt.args.t, tt.args.tname, tt.args.params, tt.args.ctx, tt.args.it); !strings.Contains(string(got), string(tt.want)) {
+				t.Errorf("AssertStructUnchanged() = \n%s\nwant: \n%s", string(got), string(tt.want))
 			}
 		})
 	}
