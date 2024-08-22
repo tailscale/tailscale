54b0951faab0cc19258ca93f45c1281d2cdc93fc
Flakes Updater <noreply+flakes-updater@tailscale.com>
2024-08-22 19:37:12 +0000

    go.mod.sri: update SRI hash for go.mod changes
    Signed-off-by: Flakes Updater <noreply+flakes-updater@tailscale.com>

--- flake.nix
+++ flake.nix
@@ -120,4 +120,4 @@
   in
     flake-utils.lib.eachDefaultSystem (system: flakeForSystem nixpkgs system);
 }
-# nix-direnv cache busting line: sha256-M5e5dE1gGW3ly94r3SxCsBmVwbBmhVtaVDW691vxG/8=
+# nix-direnv cache busting line: sha256-oWxQl5S7ZrSXB07j3JGyJHRfZU1hBnsnkAQYkcPDHg0=
