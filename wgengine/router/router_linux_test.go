catherinemabato/activity

wgengine/router: add ip rules for unifi udm-pro
Fixes: #4038

Signed-off-by: Jason Barnett <J@sonBarnett.com>

wgengine/router/router_linux_test.go
@@ -19,6 +19,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	ts_netlink "github.com/tailscale/netlink"
 	"github.com/tailscale/wireguard-go/tun"
 	"github.com/vishvananda/netlink"
 	"go4.org/netipx"
@@ -1131,4 +1132,39 @@ func adjustFwmask(t *testing.T, s string) string {
 	}
 
 	return fwmaskAdjustRe.ReplaceAllString(s, "$1")
+}
+
+func TestAddUDMProIpRules(t *testing.T) {
+	originalIpRules := make([]ts_netlink.Rule, len(_ipRules))
+	copy(originalIpRules, _ipRules)
+
+	addUDMProIpRules()
+
+	expectedRules := []ts_netlink.Rule{
+		{
+			Priority: 21,
+			Mark:     linuxfw.TailscaleBypassMarkNum,
+			Table:    udmProRouteTable1.Num,
+		},
+		{
+			Priority: 22,
+			Mark:     linuxfw.TailscaleBypassMarkNum,
+			Table:    udmProRouteTable2.Num,
+		},
+	}
+
+	for _, expected := range expectedRules {
+		found := false
+		for _, actual := range ipRules() {
+			if reflect.DeepEqual(actual, expected) {
+				found = true
+				break
+			}
+		}
+		if !found {
+			t.Errorf("Expected rule %+v; not found in ipRules", expected)
+		}
+	}
+
+	_ipRules = originalIpRules
 }

tailscale/tailscale

wgengine/router/router_linux_test.go
@@ -1231,3 +1231,38 @@ func adjustFwmask(t *testing.T, s string) string {
 
 	return fwmaskAdjustRe.ReplaceAllString(s, "$1")
 }
+
+func TestAddUDMProIpRules(t *testing.T) {
+	originalIpRules := make([]netlink.Rule, len(_ipRules))
+	copy(originalIpRules, _ipRules)
+
+	addUDMProIpRules()
+
+	expectedRules := []netlink.Rule{
+		{
+			Priority: 21,
+			Mark:     linuxfw.TailscaleBypassMarkNum,
+			Table:    udmProRouteTable1.Num,
+		},
+		{
+			Priority: 22,
+			Mark:     linuxfw.TailscaleBypassMarkNum,
+			Table:    udmProRouteTable2.Num,
+		},
+	}
+
+	for _, expected := range expectedRules {
+		found := false
+		for _, actual := range ipRules() {
+			if reflect.DeepEqual(actual, expected) {
+				found = true
+				break
+			}
+		}
+		if !found {
+			t.Errorf("Expected rule %+v; not found in ipRules", expected)
+		}
+	}
+
+	_ipRules = originalIpRules
+}