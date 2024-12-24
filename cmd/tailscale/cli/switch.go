d444d148105bb9da322a9657c92ff81910ccb007
Rob Berwick <rob.berwick@gmail.com>
2024-12-24 13:43:33 +0000

    Reference tailscale in cli switch command description
    The `switch` command's long description incorrectly references `tailnet switch -list`. Update this text so that it uses the correct form:  `tailscale switch -list`.
    
    Updates: #14468
    Signed-off-by: Rob Berwick <rob.berwick@gmail.com>

--- cmd/tailscale/cli/switch.go
+++ cmd/tailscale/cli/switch.go
@@ -22,7 +22,7 @@ var switchCmd = &ffcli.Command{
 	ShortUsage: "tailscale switch <id>",
 	ShortHelp:  "Switches to a different Tailscale account",
 	LongHelp: `"tailscale switch" switches between logged in accounts. You can
-use the ID that's returned from 'tailnet switch -list'
+use the ID that's returned from 'tailscale switch -list'
 to pick which profile you want to switch to. Alternatively, you
 can use the Tailnet or the account names to switch as well.
 
