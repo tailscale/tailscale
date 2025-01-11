0f7aecf3124c201d7ce534023649afaa56f12a20
Brad Fitzpatrick <bradfitz@tailscale.com>
2025-01-10 22:39:32 -0800

    lanscaping: remove inotify file watcher for linux dns resolv.conf
    -rwxr-xr-x@ 1 bradfitz  staff  12743346 Jan 10 22:39 /Users/bradfitz/bin/tailscaled.min
    -rwxr-xr-x@ 1 bradfitz  staff  13238424 Jan 10 22:39 /Users/bradfitz/bin/tailscaled.minlinux
    
    Change-Id: Ic69f95c0f132499ba8186aecbe6357ad2a2f6085
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- net/dns/direct_linux.go
+++ net/dns/direct_linux.go
@@ -5,55 +5,12 @@ package dns
 
 import (
 	"bytes"
-	"context"
 
-	"github.com/illarion/gonotify/v2"
 	"tailscale.com/health"
 )
 
 func (m *directManager) runFileWatcher() {
-	ctx, cancel := context.WithCancel(m.ctx)
-	defer cancel()
-	in, err := gonotify.NewInotify(ctx)
-	if err != nil {
-		// Oh well, we tried. This is all best effort for now, to
-		// surface warnings to users.
-		m.logf("dns: inotify new: %v", err)
-		return
-	}
-
-	const events = gonotify.IN_ATTRIB |
-		gonotify.IN_CLOSE_WRITE |
-		gonotify.IN_CREATE |
-		gonotify.IN_DELETE |
-		gonotify.IN_MODIFY |
-		gonotify.IN_MOVE
-
-	if err := in.AddWatch("/etc/", events); err != nil {
-		m.logf("dns: inotify addwatch: %v", err)
-		return
-	}
-	for {
-		events, err := in.Read()
-		if ctx.Err() != nil {
-			return
-		}
-		if err != nil {
-			m.logf("dns: inotify read: %v", err)
-			return
-		}
-		var match bool
-		for _, ev := range events {
-			if ev.Name == resolvConf {
-				match = true
-				break
-			}
-		}
-		if !match {
-			continue
-		}
-		m.checkForFileTrample()
-	}
+	<-m.ctx.Done()
 }
 
 var resolvTrampleWarnable = health.Register(&health.Warnable{
