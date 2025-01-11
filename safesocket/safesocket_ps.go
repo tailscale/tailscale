5d1a69822eee01dc7bd07aa209b698d59727d775
Brad Fitzpatrick <bradfitz@tailscale.com>
2025-01-11 08:02:46 -0800

    lanscaping: remove go-ps from safesocket
    -rwxr-xr-x@ 1 bradfitz  staff  11442930 Jan 11 08:02 /Users/bradfitz/bin/tailscaled.min
    -rwxr-xr-x@ 1 bradfitz  staff  11927704 Jan 11 08:02 /Users/bradfitz/bin/tailscaled.minlinux
    
    Change-Id: Ia43bd8b13aec5fc3af4557154f2daa7550b5dbfb
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- safesocket/safesocket_ps.go
+++ safesocket/safesocket_ps.go
@@ -5,30 +5,5 @@
 
 package safesocket
 
-import (
-	"strings"
-
-	ps "github.com/mitchellh/go-ps"
-)
-
 func init() {
-	tailscaledProcExists = func() bool {
-		procs, err := ps.Processes()
-		if err != nil {
-			return false
-		}
-		for _, proc := range procs {
-			name := proc.Executable()
-			const tailscaled = "tailscaled"
-			if len(name) < len(tailscaled) {
-				continue
-			}
-			// Do case insensitive comparison for Windows,
-			// notably, and ignore any ".exe" suffix.
-			if strings.EqualFold(name[:len(tailscaled)], tailscaled) {
-				return true
-			}
-		}
-		return false
-	}
 }
