51491012ecfef52a543f3aca9923156e53524d22
Brad Fitzpatrick <bradfitz@tailscale.com>
2025-01-10 21:26:39 -0800

    lanscaping: omit h2c
    -rwxr-xr-x@ 1 bradfitz  staff  13759346 Jan 10 21:26 /Users/bradfitz/bin/tailscaled.min
    -rwxr-xr-x@ 1 bradfitz  staff  15007896 Jan 10 21:26 /Users/bradfitz/bin/tailscaled.minlinux
    
    Change-Id: I523522c71deaeb4f7c2f6e81630109337682931c
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- ipn/ipnlocal/peerapi_h2c.go
+++ ipn/ipnlocal/peerapi_h2c.go
@@ -1,7 +1,7 @@
 // Copyright (c) Tailscale Inc & AUTHORS
 // SPDX-License-Identifier: BSD-3-Clause
 
-//go:build !ios && !android && !js
+//go:build !ios && !android && !js && !ts_omit_h2c
 
 package ipnlocal
 
