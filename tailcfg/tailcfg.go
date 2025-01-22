36f87e1169bdf41fd455345bebd65b1543709ead
KevinLiang10 <37811973+KevinLiang10@users.noreply.github.com>
2025-01-22 10:45:30 -0500

    delete extra struct in tailcfg
    Signed-off-by: KevinLiang10 <37811973+KevinLiang10@users.noreply.github.com>

--- tailcfg/tailcfg.go
+++ tailcfg/tailcfg.go
@@ -2997,19 +2997,3 @@ const LBHeader = "Ts-Lb"
 // correspond to those IPs. Any services that don't correspond to a service
 // this client is hosting can be ignored.
 type ServiceIPMappings map[string][]netip.Addr
-
-// IPServiceMappings maps IP addresses to service names. This is the inverse of
-// [ServiceIPMappings], and is used to inform clients which services is an VIP
-// address associated with. This is set to b.ipVIPServiceMap every time the
-// netmap is updated. This is used to reduce the cost for looking up the service
-// name for the dst IP address in the netStack packet processing workflow.
-//
-// This is of the form:
-//
-//	{
-//	  "100.65.32.1": "svc:samba",
-//	  "fd7a:115c:a1e0::1234": "svc:samba",
-//	  "100.102.42.3": "svc:web",
-//	  "fd7a:115c:a1e0::abcd": "svc:web",
-//	}
-type IPServiceMappings map[netip.Addr]string
