41dee790b9c96565fcf654bba09132b5637e0036
Jonathan Lassoff <jof@thejof.com>
2025-01-25 23:22:44 -0800

    cmd/tailscale: add a `route-table` command

--- cmd/tailscale/cli/cli.go
+++ cmd/tailscale/cli/cli.go
@@ -218,6 +218,7 @@ change in the future.
 			idTokenCmd,
 			advertiseCmd(),
 			configureHostCmd(),
+			routeTableCmd,
 		),
 		FlagSet: rootfs,
 		Exec: func(ctx context.Context, args []string) error {
