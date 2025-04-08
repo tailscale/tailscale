6da94c6997c9b907e2da853ed11b39073784c1a3
Chatnoir Miki <cmiki@amono.me>
2025-04-08 16:37:35 +0800

    go.mod.sri: update SRI hash for go.mod changes
    Signed-off-by: Chatnoir Miki <cmiki@amono.me>

--- flake.nix
+++ flake.nix
@@ -130,4 +130,4 @@
   in
     flake-utils.lib.eachDefaultSystem (system: flakeForSystem nixpkgs system);
 }
-# nix-direnv cache busting line: sha256-xO1DuLWi6/lpA9ubA2ZYVJM+CkVNA5IaVGZxX9my0j0=
+# nix-direnv cache busting line: sha256-av4kr09rjNRmag94ziNjJuI/cg8b8lAD3Tk24t/ezH4=
