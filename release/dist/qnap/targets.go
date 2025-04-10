24f75fd66de567f8ce0528df6fc83d8f62a101d4
Percy Wegmann <percy@tailscale.com>
2025-04-10 14:51:29 -0500

    WIP
    Signed-off-by: Percy Wegmann <percy@tailscale.com>

--- release/dist/qnap/targets.go
+++ release/dist/qnap/targets.go
@@ -7,30 +7,41 @@ import "tailscale.com/release/dist"
 
 // Targets defines the dist.Targets for QNAP devices.
 //
-// If privateKeyPath and certificatePath are both provided non-empty,
-// these targets will be signed for QNAP app store release with built.
-func Targets(privateKeyPath, certificatePath string) []dist.Target {
+// If all parameters are provided non-empty, then the build will be signed using
+// a Google Cloud hosted key.
+//
+// gcloudCredentialsBase64 is the JSON credential for connecting to Google Cloud, base64 encoded.
+// gcloudKeyring is the full path to the Google Cloud keyring containing the signing key.
+// keyName is the name of the key.
+// certificateBase64 is the PEM certificate to use in the signature, base64 encoded.
+func Targets(gcloudCredentialsBase64, gcloudProject, gcloudKeyring, keyName, certificateBase64 string) []dist.Target {
 	var signerInfo *signer
-	if privateKeyPath != "" && certificatePath != "" {
-		signerInfo = &signer{privateKeyPath, certificatePath}
+	if gcloudCredentialsBase64 != "" && gcloudProject != "" && gcloudKeyring != "" && keyName != "" && certificateBase64 != "" {
+		signerInfo = &signer{
+			gcloudCredentialsBase64: gcloudCredentialsBase64,
+			gcloudProject:           gcloudProject,
+			gcloudKeyring:           gcloudKeyring,
+			keyName:                 keyName,
+			certificateBase64:       certificateBase64,
+		}
 	}
 	return []dist.Target{
-		&target{
-			arch: "x86",
-			goenv: map[string]string{
-				"GOOS":   "linux",
-				"GOARCH": "386",
-			},
-			signer: signerInfo,
-		},
-		&target{
-			arch: "x86_ce53xx",
-			goenv: map[string]string{
-				"GOOS":   "linux",
-				"GOARCH": "386",
-			},
-			signer: signerInfo,
-		},
+		// &target{
+		// 	arch: "x86",
+		// 	goenv: map[string]string{
+		// 		"GOOS":   "linux",
+		// 		"GOARCH": "386",
+		// 	},
+		// 	signer: signerInfo,
+		// },
+		// &target{
+		// 	arch: "x86_ce53xx",
+		// 	goenv: map[string]string{
+		// 		"GOOS":   "linux",
+		// 		"GOARCH": "386",
+		// 	},
+		// 	signer: signerInfo,
+		// },
 		&target{
 			arch: "x86_64",
 			goenv: map[string]string{
@@ -39,37 +50,37 @@ func Targets(privateKeyPath, certificatePath string) []dist.Target {
 			},
 			signer: signerInfo,
 		},
-		&target{
-			arch: "arm-x31",
-			goenv: map[string]string{
-				"GOOS":   "linux",
-				"GOARCH": "arm",
-			},
-			signer: signerInfo,
-		},
-		&target{
-			arch: "arm-x41",
-			goenv: map[string]string{
-				"GOOS":   "linux",
-				"GOARCH": "arm",
-			},
-			signer: signerInfo,
-		},
-		&target{
-			arch: "arm-x19",
-			goenv: map[string]string{
-				"GOOS":   "linux",
-				"GOARCH": "arm",
-			},
-			signer: signerInfo,
-		},
-		&target{
-			arch: "arm_64",
-			goenv: map[string]string{
-				"GOOS":   "linux",
-				"GOARCH": "arm64",
-			},
-			signer: signerInfo,
-		},
+		// &target{
+		// 	arch: "arm-x31",
+		// 	goenv: map[string]string{
+		// 		"GOOS":   "linux",
+		// 		"GOARCH": "arm",
+		// 	},
+		// 	signer: signerInfo,
+		// },
+		// &target{
+		// 	arch: "arm-x41",
+		// 	goenv: map[string]string{
+		// 		"GOOS":   "linux",
+		// 		"GOARCH": "arm",
+		// 	},
+		// 	signer: signerInfo,
+		// },
+		// &target{
+		// 	arch: "arm-x19",
+		// 	goenv: map[string]string{
+		// 		"GOOS":   "linux",
+		// 		"GOARCH": "arm",
+		// 	},
+		// 	signer: signerInfo,
+		// },
+		// &target{
+		// 	arch: "arm_64",
+		// 	goenv: map[string]string{
+		// 		"GOOS":   "linux",
+		// 		"GOARCH": "arm64",
+		// 	},
+		// 	signer: signerInfo,
+		// },
 	}
 }
