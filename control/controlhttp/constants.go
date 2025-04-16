855300db511b78b33bda80471c48d553ae126551
hanke <hanke0@outlook.com>
2025-04-16 14:16:26 +0800

    control/controlhttp: replace default go user-agent
    Most WAF rules disable default Go user agent as it's too generic.
    This commit replacing the default Go user agent (Go-http-client) with
    "Tailscale/<version>".

--- control/controlhttp/constants.go
+++ control/controlhttp/constants.go
@@ -17,6 +17,7 @@ import (
 	"tailscale.com/tstime"
 	"tailscale.com/types/key"
 	"tailscale.com/types/logger"
+	"tailscale.com/version"
 )
 
 const (
@@ -113,4 +114,8 @@ func strDef(v1, v2 string) string {
 		return v1
 	}
 	return v2
+}
+
+func getUserAgent() string {
+	return "Tailscale/" + version.Short()
 }
