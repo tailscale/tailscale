15a8d9ab56b9afbf9e11baa202b7b220980b92f1
Tom Proctor <tomhjp@users.noreply.github.com>
2025-05-01 18:13:54 +0100

    cmd/k8s-operator,k8s-operator/api-proxy: move k8s proxy code to library
    The defaultEnv and defaultBool functions are copied over temporarily
    to minimise diff. This lays the ground work for having both the operator
    and the new k8s-proxy binary implement the API proxy
    
    Updates #13358
    
    Change-Id: Ieacc79af64df2f13b27a18135517bb31c80a5a02
    Signed-off-by: Tom Proctor <tomhjp@users.noreply.github.com>

---
+++ k8s-operator/api-proxy/doc.go
@@ -0,0 +1,8 @@
+// Copyright (c) Tailscale Inc & AUTHORS
+// SPDX-License-Identifier: BSD-3-Clause
+
+//go:build !plan9
+
+// Package apiproxy contains the Kubernetes API Proxy implementation used by
+// k8s-operator and k8s-proxy.
+package apiproxy
---
+++ k8s-operator/api-proxy/env.go
@@ -0,0 +1,24 @@
+package apiproxy
+
+import (
+	"os"
+
+	"tailscale.com/types/opt"
+)
+
+func defaultBool(envName string, defVal bool) bool {
+	vs := os.Getenv(envName)
+	if vs == "" {
+		return defVal
+	}
+	v, _ := opt.Bool(vs).Get()
+	return v
+}
+
+func defaultEnv(envName, defVal string) string {
+	v := os.Getenv(envName)
+	if v == "" {
+		return defVal
+	}
+	return v
+}
