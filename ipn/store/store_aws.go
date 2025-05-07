fd263adc1b5b75ad78205a439cbbe5a505a90014
Brad Fitzpatrick <bradfitz@tailscale.com>
2025-05-07 09:26:28 -0700

    ipn/store: don't link in AWS & Kubernetes stuff on Android
    Android is Linux, but that not much Linux.
    
    Updates #12614
    
    Change-Id: Ice80bd3e3d173511c30d05a43d25a31e18928db7
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- ipn/store/store_aws.go
+++ ipn/store/store_aws.go
@@ -1,7 +1,7 @@
 // Copyright (c) Tailscale Inc & AUTHORS
 // SPDX-License-Identifier: BSD-3-Clause
 
-//go:build (ts_aws || (linux && (arm64 || amd64))) && !ts_omit_aws
+//go:build (ts_aws || (linux && (arm64 || amd64) && !android)) && !ts_omit_aws
 
 package store
 
