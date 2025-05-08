2917d5da95cd11c68e62511d29594c2e17695dda
Brad Fitzpatrick <bradfitz@tailscale.com>
2025-05-07 20:27:30 -0700

    all: remove non-applicable "linux" deps on Android
    Updates #12614
    
    Change-Id: I0e2a18eca3515d3d6206c059110556d2bbbb0c5c
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- safesocket/safesocket_ps.go
+++ safesocket/safesocket_ps.go
@@ -1,7 +1,7 @@
 // Copyright (c) Tailscale Inc & AUTHORS
 // SPDX-License-Identifier: BSD-3-Clause
 
-//go:build linux || windows || (darwin && !ios) || freebsd
+//go:build (linux && !android) || windows || (darwin && !ios) || freebsd
 
 package safesocket
 
