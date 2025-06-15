16953f48eef0b4a9da5db50bc1934818ef549970
Brad Fitzpatrick <bradfitz@tailscale.com>
2025-06-15 08:25:36 -0700

    tool/gocross: make gocross opt-in instead of opt-out
    gocross is not needed like it used to be, now that Go does
    version stamping itself.
    
    We keep it for the xcode and Windows builds for now.
    
    This simplifies things in the build, especially with upcoming build
    system updates.
    
    Updates tailscale/corp#28679
    Updates tailscale/corp#26717
    
    Change-Id: Ib4bebe6f50f3b9c3d6cd27323fca603e3dfb43cc
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- tool/gocross/gocross-wrapper.sh
+++ tool/gocross/gocross-wrapper.sh
@@ -3,8 +3,11 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # gocross-wrapper.sh is a wrapper that can be aliased to 'go', which
-# transparently builds gocross using a "bootstrap" Go toolchain, and
-# then invokes gocross.
+# transparently runs the version of github.com/tailscale/go as specified repo's
+# go.toolchain.rev file.
+#
+# It also conditionally (if TS_USE_GOCROSS=1) builds gocross and uses it as a go
+# wrapper to inject certain go flags.
 
 set -euo pipefail
 
@@ -75,6 +78,14 @@ case "$REV" in
     fi
     ;;
 esac
+
+# gocross is opt-in as of 2025-06-16. See tailscale/corp#26717.
+# It's primarily used for xcode builds, and a bit still for Windows.
+# In the past we needed it for git version stamping on Linux etc, but
+# Go does that itself nowadays.
+if [ "${TS_USE_GOCROSS:-}" != "1" ]; then
+    exec "$toolchain/bin/go" "$@"
+fi
 
 if [[ -d "$toolchain" ]]; then
     # A toolchain exists, but is it recent enough to compile gocross? If not,
