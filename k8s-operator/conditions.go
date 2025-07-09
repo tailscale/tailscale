63fbe63357cce6c1a7a6108073b659d6b9a03d2e
Tom Proctor <tomhjp@users.noreply.github.com>
2025-07-09 02:35:23 +0100

    cmd/k8s-operator: don't require generation for Available condition
    The observed generation was set to always 0 in #16429, but this had the
    knock-on effect of other controllers considering ProxyGroups never ready
    because the observed generation is never up to date in
    proxyGroupCondition. Make sure the ProxyGroupAvailable function does not
    requires the observed generation to be up to date, and add testing
    coverage to catch regressions.
    
    Updates #16327
    
    Change-Id: I42f50ad47dd81cc2d3c3ce2cd7b252160bb58e40
    Signed-off-by: Tom Proctor <tomhjp@users.noreply.github.com>

--- k8s-operator/conditions.go
+++ k8s-operator/conditions.go
@@ -137,22 +137,23 @@ func ProxyClassIsReady(pc *tsapi.ProxyClass) bool {
 }
 
 func ProxyGroupIsReady(pg *tsapi.ProxyGroup) bool {
-	return proxyGroupCondition(pg, tsapi.ProxyGroupReady)
+	cond := proxyGroupCondition(pg, tsapi.ProxyGroupReady)
+	return cond != nil && cond.Status == metav1.ConditionTrue && cond.ObservedGeneration == pg.Generation
 }
 
 func ProxyGroupAvailable(pg *tsapi.ProxyGroup) bool {
-	return proxyGroupCondition(pg, tsapi.ProxyGroupAvailable)
+	cond := proxyGroupCondition(pg, tsapi.ProxyGroupAvailable)
+	return cond != nil && cond.Status == metav1.ConditionTrue
 }
 
-func proxyGroupCondition(pg *tsapi.ProxyGroup, condType tsapi.ConditionType) bool {
+func proxyGroupCondition(pg *tsapi.ProxyGroup, condType tsapi.ConditionType) *metav1.Condition {
 	idx := xslices.IndexFunc(pg.Status.Conditions, func(cond metav1.Condition) bool {
 		return cond.Type == string(condType)
 	})
 	if idx == -1 {
-		return false
+		return nil
 	}
-	cond := pg.Status.Conditions[idx]
-	return cond.Status == metav1.ConditionTrue && cond.ObservedGeneration == pg.Generation
+	return &pg.Status.Conditions[idx]
 }
 
 func DNSCfgIsReady(cfg *tsapi.DNSConfig) bool {
