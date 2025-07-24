278311db1b445e4bdc4f2a3902684f639b4156bc
Tom Proctor <tomhjp@users.noreply.github.com>
2025-07-24 20:33:24 +0100

    k8s-operator: handle multiple WebSocket frames per read
    When kubectl starts an interactive attach session, it sends 2 resize
    messages in quick succession. It seems that particularly in HTTP mode,
    we often receive both of these WebSocket frames from the underlying
    connection in a single read. However, our parser currently assumes 0-1
    frames per read, and leaves the second frame in the read buffer until
    the next read from the underlying connection. It doesn't take long after
    that before we end up failing to skip a control message as we normally
    should, and then we parse a control message as though it will have a
    stream ID (part of the Kubernetes protocol) and error out.
    
    Instead, we should keep parsing frames from the read buffer for as long
    as we're able to parse complete frames, so this commit refactors the
    messages parsing logic into a loop based on the contents of the read
    buffer being non-empty.
    
    k/k staging/src/k8s.io/kubectl/pkg/cmd/attach/attach.go for full
    details of the resize messages.
    
    There are at least a couple more multiple-frame read edge cases we
    should handle, but this commit is very conservatively fixing a single
    observed issue to make it a low-risk candidate for cherry picking.
    
    Updates #13358
    
    Change-Id: Iafb91ad1cbeed9c5231a1525d4563164fc1f002f
    Signed-off-by: Tom Proctor <tomhjp@users.noreply.github.com>

--- k8s-operator/sessionrecording/ws/conn.go
+++ k8s-operator/sessionrecording/ws/conn.go
@@ -148,6 +148,8 @@ func (c *conn) Read(b []byte) (int, error) {
 		return 0, nil
 	}
 
+	// TODO(tomhjp): If we get multiple frames in a single Read with different
+	// types, we may parse the second frame with the wrong type.
 	typ := messageType(opcode(b))
 	if (typ == noOpcode && c.readMsgIsIncomplete()) || c.readBufHasIncompleteFragment() { // subsequent fragment
 		if typ, err = c.curReadMsgType(); err != nil {
@@ -157,6 +159,8 @@ func (c *conn) Read(b []byte) (int, error) {
 
 	// A control message can not be fragmented and we are not interested in
 	// these messages. Just return.
+	// TODO(tomhjp): If we get multiple frames in a single Read, we may skip
+	// some non-control messages.
 	if isControlMessage(typ) {
 		return n, nil
 	}
@@ -169,62 +173,65 @@ func (c *conn) Read(b []byte) (int, error) {
 		return n, nil
 	}
 
-	readMsg := &message{typ: typ} // start a new message...
-	// ... or pick up an already started one if the previous fragment was not final.
-	if c.readMsgIsIncomplete() || c.readBufHasIncompleteFragment() {
-		readMsg = c.currentReadMsg
-	}
-
 	if _, err := c.readBuf.Write(b[:n]); err != nil {
 		return 0, fmt.Errorf("[unexpected] error writing message contents to read buffer: %w", err)
 	}
 
-	ok, err := readMsg.Parse(c.readBuf.Bytes(), c.log)
-	if err != nil {
-		return 0, fmt.Errorf("error parsing message: %v", err)
-	}
-	if !ok { // incomplete fragment
-		return n, nil
-	}
-	c.readBuf.Next(len(readMsg.raw))
+	for c.readBuf.Len() != 0 {
+		readMsg := &message{typ: typ} // start a new message...
+		// ... or pick up an already started one if the previous fragment was not final.
+		if c.readMsgIsIncomplete() {
+			readMsg = c.currentReadMsg
+		}
 
-	if readMsg.isFinalized && !c.readMsgIsIncomplete() {
-		// we want to send stream resize messages for terminal sessions
-		// Stream IDs for websocket streams are static.
-		// https://github.com/kubernetes/client-go/blob/v0.30.0-rc.1/tools/remotecommand/websocket.go#L218
-		if readMsg.streamID.Load() == remotecommand.StreamResize && c.hasTerm {
-			var msg tsrecorder.ResizeMsg
-			if err = json.Unmarshal(readMsg.payload, &msg); err != nil {
-				return 0, fmt.Errorf("error umarshalling resize message: %w", err)
-			}
+		ok, err := readMsg.Parse(c.readBuf.Bytes(), c.log)
+		if err != nil {
+			return 0, fmt.Errorf("error parsing message: %v", err)
+		}
+		if !ok { // incomplete fragment
+			return n, nil
+		}
+		c.readBuf.Next(len(readMsg.raw))
 
-			c.ch.Width = msg.Width
-			c.ch.Height = msg.Height
+		if readMsg.isFinalized && !c.readMsgIsIncomplete() {
+			// we want to send stream resize messages for terminal sessions
+			// Stream IDs for websocket streams are static.
+			// https://github.com/kubernetes/client-go/blob/v0.30.0-rc.1/tools/remotecommand/websocket.go#L218
+			if readMsg.streamID.Load() == remotecommand.StreamResize && c.hasTerm {
+				var msg tsrecorder.ResizeMsg
+				if err = json.Unmarshal(readMsg.payload, &msg); err != nil {
+					return 0, fmt.Errorf("error umarshalling resize message: %w", err)
+				}
 
-			var isInitialResize bool
-			c.writeCastHeaderOnce.Do(func() {
-				isInitialResize = true
-				// If this is a session with a terminal attached,
-				// we must wait for the terminal width and
-				// height to be parsed from a resize message
-				// before sending CastHeader, else tsrecorder
-				// will not be able to play this recording.
-				err = c.rec.WriteCastHeader(c.ch)
-				close(c.initialCastHeaderSent)
-			})
-			if err != nil {
-				return 0, fmt.Errorf("error writing CastHeader: %w", err)
-			}
+				c.ch.Width = msg.Width
+				c.ch.Height = msg.Height
 
-			if !isInitialResize {
-				if err := c.rec.WriteResize(msg.Height, msg.Width); err != nil {
-					return 0, fmt.Errorf("error writing resize message: %w", err)
+				var isInitialResize bool
+				c.writeCastHeaderOnce.Do(func() {
+					isInitialResize = true
+					// If this is a session with a terminal attached,
+					// we must wait for the terminal width and
+					// height to be parsed from a resize message
+					// before sending CastHeader, else tsrecorder
+					// will not be able to play this recording.
+					err = c.rec.WriteCastHeader(c.ch)
+					close(c.initialCastHeaderSent)
+				})
+				if err != nil {
+					return 0, fmt.Errorf("error writing CastHeader: %w", err)
+				}
+
+				if !isInitialResize {
+					if err := c.rec.WriteResize(msg.Height, msg.Width); err != nil {
+						return 0, fmt.Errorf("error writing resize message: %w", err)
+					}
 				}
 			}
 		}
+
+		c.currentReadMsg = readMsg
 	}
 
-	c.currentReadMsg = readMsg
 	return n, nil
 }
 
