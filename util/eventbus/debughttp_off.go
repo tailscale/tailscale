ffc82ad82014b03533e7214a2b259e62801d2191
Brad Fitzpatrick <bradfitz@tailscale.com>
2025-09-08 09:33:39 -0700

    util/eventbus: add ts_omit_debugeventbus
    Updates #17063
    
    Change-Id: Ibc98dd2088f82c829effa71f72f3e2a5abda5038
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- util/eventbus/debughttp_off.go
+++ util/eventbus/debughttp_off.go
@@ -1,7 +1,7 @@
 // Copyright (c) Tailscale Inc & AUTHORS
 // SPDX-License-Identifier: BSD-3-Clause
 
-//go:build ios || android
+//go:build ios || android || ts_omit_debugeventbus
 
 package eventbus
 
