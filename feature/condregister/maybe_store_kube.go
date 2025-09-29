69c79cb9f3f9e2fe9ce4333c9a034591709e469b
Brad Fitzpatrick <bradfitz@tailscale.com>
2025-09-29 15:26:24 -0700

    ipn/store, feature/condregister: move AWS + Kube store registration to condregister
    Otherwise they're uselessly imported by tsnet applications, even
    though they do nothing. tsnet applications wanting to use these
    already had to explicitly import them and use kubestore.New or
    awsstore.New and assign those to their tsnet.Server.Store fields.
    
    Updates #12614
    
    Change-Id: I358e3923686ddf43a85e6923c3828ba2198991d4
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- feature/condregister/maybe_store_kube.go
+++ feature/condregister/maybe_store_kube.go
@@ -3,18 +3,19 @@
 
 //go:build (ts_kube || (linux && (arm64 || amd64) && !android)) && !ts_omit_kube
 
-package store
+package condregister
 
 import (
 	"strings"
 
 	"tailscale.com/ipn"
+	"tailscale.com/ipn/store"
 	"tailscale.com/ipn/store/kubestore"
 	"tailscale.com/types/logger"
 )
 
 func init() {
-	Register("kube:", func(logf logger.Logf, path string) (ipn.StateStore, error) {
+	store.Register("kube:", func(logf logger.Logf, path string) (ipn.StateStore, error) {
 		secretName := strings.TrimPrefix(path, "kube:")
 		return kubestore.New(logf, secretName)
 	})
