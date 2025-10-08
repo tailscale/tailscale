331d25301aeba5d8df4311bee5a8e44b7b2510ec
Mike O'Driscoll <mikeo@tailscale.com>
2025-10-08 12:34:18 -0400

    flake.nix: update Nix to use tailscale/go 1.25.2
    Update Nix flake to use go 1.25.2
    Create the hash from the toolchain rev file automatically from
    update-flake.sh
    
    Updates tailscale/go#135
    
    Signed-off-by: Mike O'Driscoll <mikeo@tailscale.com>

--- update-flake.sh
+++ update-flake.sh
@@ -10,6 +10,14 @@ rm -rf "$OUT"
 ./tool/go run tailscale.com/cmd/nardump --sri "$OUT" >go.mod.sri
 rm -rf "$OUT"
 
+GOOUT=$(mktemp -d -t gocross-XXXXXX)
+GOREV=$(xargs < ./go.toolchain.rev)
+TARBALL="$GOOUT/go-$GOREV.tar.gz"
+curl -Ls -o "$TARBALL" "https://github.com/tailscale/go/archive/$GOREV.tar.gz"
+tar -xzf "$TARBALL" -C "$GOOUT"
+./tool/go run tailscale.com/cmd/nardump --sri "$GOOUT/go-$GOREV" > go.toolchain.rev.sri
+rm -rf "$GOOUT"
+
 # nix-direnv only watches the top-level nix file for changes. As a
 # result, when we change a referenced SRI file, we have to cause some
 # change to shell.nix and flake.nix as well, so that nix-direnv
