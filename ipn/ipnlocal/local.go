18941c06db1f8af0a7fe0419224d535009471123
Patrick O'Doherty <patrick@tailscale.com>
2025-10-10 12:01:36 -0700

    ipn/ipnlocal: strip AttestationKey in redacted prefs view
    Updates tailscale/corp#31269
    
    Signed-off-by: Patrick O'Doherty <patrick@tailscale.com>

--- ipn/ipnlocal/local.go
+++ ipn/ipnlocal/local.go
@@ -1216,6 +1216,7 @@ func stripKeysFromPrefs(p ipn.PrefsView) ipn.PrefsView {
 	p2.Persist.PrivateNodeKey = key.NodePrivate{}
 	p2.Persist.OldPrivateNodeKey = key.NodePrivate{}
 	p2.Persist.NetworkLockKey = key.NLPrivate{}
+	p2.Persist.AttestationKey = nil
 	return p2.View()
 }
 
