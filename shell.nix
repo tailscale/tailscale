5b40f0bc547701f461605a418d49a20a0edc9f8b
Joe Tsai <joetsai@digital-static.net>
2025-11-05 16:17:49 -0800

    cmd/vet: add static vet checker that runs jsontags (#17778)
    This starts running the jsontags vet checker on the module.
    All existing findings are adding to an allowlist.
    
    Updates tailscale/corp#791
    
    Signed-off-by: Joe Tsai <joetsai@digital-static.net>

--- shell.nix
+++ shell.nix
@@ -16,4 +16,4 @@
 ) {
   src =  ./.;
 }).shellNix
-# nix-direnv cache busting line: sha256-D0znIEcy9d822snZbdNCNLoN47cOP1F2SKmfwSFRvXw=
+# nix-direnv cache busting line: sha256-p8dVVZm2bLL4J/d4TtnUOp3bfMqUkBGE+53RUhamF+A=
