00987cb1681951052ed544b72695ba5d5e0ff612
Brad Fitzpatrick <bradfitz@tailscale.com>
2025-11-19 10:54:42 -0800

    syncs: add means of declare locking assumptions for debug mode validation
    Updates #17852
    
    Change-Id: I42a64a990dcc8f708fa23a516a40731a19967aba
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- syncs/mutex.go
+++ syncs/mutex.go
@@ -16,3 +16,8 @@ type Mutex = sync.Mutex
 //
 // It's only not a sync.RWMutex when built with the ts_mutex_debug build tag.
 type RWMutex = sync.RWMutex
+
+// RequiresMutex declares the caller assumes it has the given
+// mutex held. In non-debug builds, it's a no-op and compiles to
+// nothing.
+func RequiresMutex(mu *sync.Mutex) {}
