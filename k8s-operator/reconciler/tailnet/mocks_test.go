c9dd86495ad9a0df134e32911bdc14dd72c54b7b
David Bond <davidsbond93@gmail.com>
2025-12-04 14:42:45 +0000

    cmd/k8s-operator: add reconciler for Tailnet resource
    This commit adds the new reconciler for the Tailnet resource in a new
    location that we want to put all future reconcilers in and eventually
    refactor existing reconcilers into.
    
    The Tailnet resource represents OAuth credentials for a single tailnet and
    uses status fields to indicate the validity of the credentials. Validity
    is determined via expected fields existing (client_id and client_secret)
    and API calls for devices, keys & services being possible using them.
    
    Subsequent resources will be updated to refer to a Tailnet as the source
    of their oauth credentials, or fall back to the configured default.
    
    Fixes: https://github.com/tailscale/corp/issues/34767
    Signed-off-by: David Bond <davidsbond93@gmail.com>

---
+++ k8s-operator/reconciler/tailnet/mocks_test.go
@@ -0,0 +1,43 @@
+// Copyright (c) Tailscale Inc & AUTHORS
+// SPDX-License-Identifier: BSD-3-Clause
+
+package tailnet_test
+
+import (
+	"context"
+	"io"
+
+	"tailscale.com/internal/client/tailscale"
+)
+
+type (
+	MockTailnetClient struct {
+		ErrorOnDevices  bool
+		ErrorOnKeys     bool
+		ErrorOnServices bool
+	}
+)
+
+func (m MockTailnetClient) Devices(_ context.Context, _ *tailscale.DeviceFieldsOpts) ([]*tailscale.Device, error) {
+	if m.ErrorOnDevices {
+		return nil, io.EOF
+	}
+
+	return nil, nil
+}
+
+func (m MockTailnetClient) Keys(_ context.Context) ([]string, error) {
+	if m.ErrorOnKeys {
+		return nil, io.EOF
+	}
+
+	return nil, nil
+}
+
+func (m MockTailnetClient) ListVIPServices(_ context.Context) (*tailscale.VIPServiceList, error) {
+	if m.ErrorOnServices {
+		return nil, io.EOF
+	}
+
+	return nil, nil
+}
