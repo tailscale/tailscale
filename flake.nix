5db95ec376b00a8db90bcb0c61c452f0b49a8633
Patrick O'Doherty <patrick@tailscale.com>
2026-01-09 12:16:53 -0800

    go.mod: bump github.com/containerd/containerd@v1.7.29 (#18374)
    Updates #cleanup
    
    Signed-off-by: Patrick O'Doherty <patrick@tailscale.com>

--- flake.nix
+++ flake.nix
@@ -151,4 +151,4 @@
     });
   };
 }
-# nix-direnv cache busting line: sha256-knSIes9pFVkVfK5hcBG9BSR1ueH+yPpx4hv/UsyaW2M=
+# nix-direnv cache busting line: sha256-MKMLpGUYzUPYKjVYQSnxDQDdH1oXaM8bCIbhCTuGeV0=
