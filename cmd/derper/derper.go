17b0c7bfb384892ed8a6b5c6aa669e866d493fdc
Anton Tolchanov <anton@tailscale.com>
2026-01-12 15:09:04 -0500

    metrics: add a NewLabelMap helper to create and register label maps
    Updates tailscale/corp#31174
    
    Signed-off-by: Anton Tolchanov <anton@tailscale.com>

--- cmd/derper/derper.go
+++ cmd/derper/derper.go
@@ -99,17 +99,12 @@ var (
 )
 
 var (
-	tlsRequestVersion = &metrics.LabelMap{Label: "version"}
-	tlsActiveVersion  = &metrics.LabelMap{Label: "version"}
+	tlsRequestVersion = metrics.NewLabelMap("derper_tls_request_version", "version")
+	tlsActiveVersion  = metrics.NewLabelMap("gauge_derper_tls_active_version", "version")
 )
 
 const setecMeshKeyName = "meshkey"
 const meshKeyEnvVar = "TAILSCALE_DERPER_MESH_KEY"
-
-func init() {
-	expvar.Publish("derper_tls_request_version", tlsRequestVersion)
-	expvar.Publish("gauge_derper_tls_active_version", tlsActiveVersion)
-}
 
 type config struct {
 	PrivateKey key.NodePrivate
