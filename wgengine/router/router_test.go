a38c1f27731f4246da07bd17d16f59868b01804e
Manasseh Zhou <2705803+ManassehZhou@users.noreply.github.com>
2026-02-12 01:23:08 +0800

    net,wgengine: add support for configuring bypass marks
    This change enables dynamic configuration of Linux packet marks (bypass,
    subnet route, and fwmark mask) throughout the routing and networking
    stack, moving away from hardcoded constants.
    
    Fixes #591
    
    RELNOTE: Add advanced flags to customize Linux packet marks for routing.
    
    Signed-off-by: Manasseh Zhou <2705803+ManassehZhou@users.noreply.github.com>

--- wgengine/router/router_test.go
+++ wgengine/router/router_test.go
@@ -15,7 +15,7 @@ func TestConfigEqual(t *testing.T) {
 	testedFields := []string{
 		"LocalAddrs", "Routes", "LocalRoutes", "NewMTU",
 		"SubnetRoutes", "SNATSubnetRoutes", "StatefulFiltering",
-		"NetfilterMode", "NetfilterKind", "RemoveCGNATDropRule",
+		"NetfilterMode", "NetfilterKind", "RemoveCGNATDropRule", "LinuxPacketMarks",
 	}
 	configType := reflect.TypeFor[Config]()
 	configFields := []string{}
