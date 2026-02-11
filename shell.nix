e27ddd3505ea26568110b98fe5d1259eb419073f
Brad Fitzpatrick <bradfitz@tailscale.com>
2026-02-11 20:57:56 +0000

    cmd/cigocacher: remove Windows-specific disk code moved upstream
    Updates tailscale/corp#10808
    Updates bradfitz/go-tool-cache#27
    
    Change-Id: I27a2af63d882d916998933521f17e410692255ca
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- shell.nix
+++ shell.nix
@@ -16,4 +16,4 @@
 ) {
   src =  ./.;
 }).shellNix
-# nix-direnv cache busting line: sha256-5A6EShJ33yHQdr6tgsNCRFLvNUUjIKXDv5DvzsiUwFI=
+# nix-direnv cache busting line: sha256-Yhauc4JRCbLlT/8Hn2iA2Cf7IaudwzOqu7GSEQy5r+M=
