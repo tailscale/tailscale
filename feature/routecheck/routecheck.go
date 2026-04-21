1a521b63cc3c45149444aa777ff5affcd95e9e8e
Simon Law <sfllaw@tailscale.com>
2026-04-21 10:01:15 -0700

    feature/routecheck: add ts_omit_routecheck feature flag
    RouteCheck, which checks that overlapping routers are reachable, is
    enabled by default for both tailscaled and tsnet.
    
    Updates #17366
    Updates tailscale/corp#33033
    
    Signed-off-by: Simon Law <sfllaw@tailscale.com>

---
+++ feature/routecheck/routecheck.go
@@ -0,0 +1,11 @@
+// Copyright (c) Tailscale Inc & contributors
+// SPDX-License-Identifier: BSD-3-Clause
+
+//go:build !ts_omit_routecheck
+
+// Package routecheck registers support for RouteCheck,
+// which checks the reachability of overlapping routers.
+package routecheck
+
+func init() {
+}
