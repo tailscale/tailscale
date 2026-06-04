2b727569836c64c961d54e6d8a0d599e25bff9e5
Simon Law <sfllaw@tailscale.com>
2026-06-03 19:11:33 -0700

    cmd/tailscale/cli/jsonoutput: provide examples for jsonoutput.DNS*
    This patch adds examples for unmarshalling the JSON outputs of the
    following commands:
    
    	tailscale dns query --json
    	tailscale dns status --json
    
    Updates #18750
    
    Signed-off-by: Simon Law <sfllaw@tailscale.com>

---
+++ cmd/tailscale/cli/jsonoutput/example_dnsstatusresult_test.go
@@ -0,0 +1,26 @@
+// Copyright (c) Tailscale Inc & contributors
+// SPDX-License-Identifier: BSD-3-Clause
+
+package jsonoutput_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"os/exec"
+
+	"tailscale.com/cmd/tailscale/cli/jsonoutput"
+)
+
+func ExampleDNSStatusResult() {
+	cmd := exec.Command("tailscale", "dns", "status", "--json")
+	out, err := cmd.Output()
+	if err != nil {
+		panic(err)
+	}
+
+	var dnsStatus jsonoutput.DNSStatusResult
+	if err := json.Unmarshal(out, &dnsStatus); err != nil {
+		panic(err)
+	}
+	fmt.Printf("{accept-dns: %t, resolvers: %q}\n", dnsStatus.TailscaleDNS, dnsStatus.Resolvers)
+}
