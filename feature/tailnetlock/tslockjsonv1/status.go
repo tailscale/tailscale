7175f6a4583eeab482762d98ad75f36e542fa475
Simon Law <sfllaw@tailscale.com>
2026-06-04 19:28:12 -0700

    feature/tailnetlock/tslockjsonv1: turn print functions into JSON converters
    This patch pulls the printing and JSON-encoding out of
    feature/tailnetlock/tslockjsonv1 into their callers, so that this
    package only handles type conversions.
    
    In cmd/tailscale/cli/tailnet-lock.go, it extracts the
    printTailnetLockStatus function from runTailnetLockStatus to mirror
    printTailnetLockLog and runTailnetLockLog.
    
    Updates #17613
    
    Signed-off-by: Simon Law <sfllaw@tailscale.com>

--- feature/tailnetlock/tslockjsonv1/status.go
+++ feature/tailnetlock/tslockjsonv1/status.go
@@ -6,38 +6,19 @@
 package tslockjsonv1
 
 import (
-	jsonv1 "encoding/json"
-	"io"
-
 	"tailscale.com/cmd/tailscale/jsonoutput"
 	"tailscale.com/cmd/tailscale/tslockjsonv1"
 	"tailscale.com/ipn/ipnstate"
 	"tailscale.com/tka"
 )
 
-// PrintTailnetLockStatusJSONV1 prints the current Tailnet Lock status
-// as a JSON object to the CLI, in a stable "v1" format.
-func PrintTailnetLockStatusJSONV1(out io.Writer, status *ipnstate.TailnetLockStatus) error {
-	responseEnvelope := jsonoutput.ResponseEnvelope{
-		SchemaVersion: "1",
-	}
-
-	var result tslockjsonv1.StatusResponse
-	if status.Enabled {
-		result = toTailnetLockEnabledStatusV1(status)
-		result.ResponseEnvelope = responseEnvelope
-	} else {
-		result = toTailnetLockDisabledStatusV1(status)
-		result.ResponseEnvelope = responseEnvelope
-	}
-
-	enc := jsonv1.NewEncoder(out)
-	enc.SetIndent("", "  ")
-	return enc.Encode(result)
-}
-
-func toTailnetLockDisabledStatusV1(status *ipnstate.TailnetLockStatus) tslockjsonv1.StatusResponse {
+// StatusResponse returns the current Tailnet Lock status as a JSON object to the CLI,
+// in a stable "v1" format.
+func StatusResponse(status *ipnstate.TailnetLockStatus) tslockjsonv1.StatusResponse {
 	out := tslockjsonv1.StatusResponse{
+		ResponseEnvelope: jsonoutput.ResponseEnvelope{
+			SchemaVersion: "1",
+		},
 		Enabled: status.Enabled,
 	}
 	if !status.PublicKey.IsZero() {
@@ -46,12 +27,8 @@ func toTailnetLockDisabledStatusV1(status *ipnstate.TailnetLockStatus) tslockjso
 	if nk := status.NodeKey; nk != nil {
 		out.NodeKey = nk.String()
 	}
-	return out
-}
-
-func toTailnetLockEnabledStatusV1(status *ipnstate.TailnetLockStatus) tslockjsonv1.StatusResponse {
-	out := tslockjsonv1.StatusResponse{
-		Enabled: status.Enabled,
+	if !status.Enabled {
+		return out // Tailnet Lock is disabled, omit the following fields
 	}
 
 	if status.Head != nil {
@@ -59,12 +36,6 @@ func toTailnetLockEnabledStatusV1(status *ipnstate.TailnetLockStatus) tslockjson
 		h := status.Head
 		copy(head[:], h[:])
 		out.Head = head.String()
-	}
-	if !status.PublicKey.IsZero() {
-		out.PublicKey = status.PublicKey.CLIString()
-	}
-	if nk := status.NodeKey; nk != nil {
-		out.NodeKey = nk.String()
 	}
 	out.NodeKeySigned = &status.NodeKeySigned
 	if sig := status.NodeKeySignature; sig != nil {
@@ -83,6 +54,5 @@ func toTailnetLockEnabledStatusV1(status *ipnstate.TailnetLockStatus) tslockjson
 		out.FilteredPeers = append(out.FilteredPeers, peer(fp))
 	}
 	out.StateID = status.StateID
-
 	return out
 }
