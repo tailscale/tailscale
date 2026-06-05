42a3f151d379879673c88a5fa8c76b08d1c45902
Simon Law <sfllaw@tailscale.com>
2026-06-04 21:10:26 -0700

    cmd/tailscale/jsonoutput: expand on package documentation
    Updates #17613
    
    Signed-off-by: Simon Law <sfllaw@tailscale.com>

--- cmd/tailscale/tslockjsonv1/doc.go
+++ cmd/tailscale/tslockjsonv1/doc.go
@@ -4,5 +4,8 @@
 //go:build !ts_omit_tailnetlock
 
 // Package tslockjsonv1 provides types for unmarshaling the JSON output of the
-// "tailscale lock --json=1" command.
+// "tailscale lock --json=1" command:
+//
+//   - [LogResponse] will unmarshal the output of "tailscale lock log --json=1"
+//   - [StatusResponse] will unmarshal the output of "tailscale lock status --json=1".
 package tslockjsonv1
