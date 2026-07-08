460520b70cc61b4cd5ff9489d7ac887706fe3811
David Bond <davidsbond93@gmail.com>
2026-07-08 10:42:37 +0100

    k8s-operator/reconciler: deploy StatefulSet for PeerRelay
    This commit modifies the `PeerRelay` reconciler to deploy a `StatefulSet`
    using the tailscale image that is configured to act as a peer relay based
    on the `LoadBalancer` type `Service`'s IP addresses.
    
    It also introduces `k8s-operator/reconciler/tailscaled` which is intended
    to be the one-stop-shop for deploying multi-replica tailscale container
    instances that other resources can utilise when they are migrated over
    to the new package structure.
    
    Signed-off-by: David Bond <davidsbond93@gmail.com>
    Updates: https://github.com/tailscale/corp/issues/43414

---
+++ k8s-operator/reconciler/peerrelay/statefulset.go
@@ -0,0 +1,75 @@
+// Copyright (c) Tailscale Inc & contributors
+// SPDX-License-Identifier: BSD-3-Clause
+
+//go:build !plan9
+
+package peerrelay
+
+import (
+	"fmt"
+	"net/netip"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+
+	"tailscale.com/ipn"
+	tsapi "tailscale.com/k8s-operator/apis/v1alpha1"
+	"tailscale.com/k8s-operator/reconciler/tailscaled"
+)
+
+func configSecretName(prName string, idx int32) string {
+	return fmt.Sprintf("%s-%d-config", prName, idx)
+}
+
+func peerRelayHostname(pr *tsapi.PeerRelay, idx int32) string {
+	prefix := string(pr.Spec.HostnamePrefix)
+	if prefix == "" {
+		prefix = pr.Name
+	}
+	return fmt.Sprintf("%s-%d", prefix, idx)
+}
+
+func peerRelayTailscaledConfig(pr *tsapi.PeerRelay, idx int32, endpoint *tsapi.PeerRelayEndpoint) ipn.ConfigVAlpha {
+	conf := ipn.ConfigVAlpha{
+		Version:         "alpha0",
+		AcceptDNS:       "false",
+		AcceptRoutes:    "false",
+		Locked:          "false",
+		Hostname:        new(peerRelayHostname(pr, idx)),
+		RelayServerPort: new(uint16(servicePort)),
+	}
+
+	if endpoint != nil {
+		if addr, err := netip.ParseAddr(endpoint.Address); err == nil {
+			conf.RelayServerStaticEndpoints = []netip.AddrPort{
+				netip.AddrPortFrom(addr, uint16(endpoint.Port)),
+			}
+		}
+	}
+
+	return conf
+}
+
+func (r *Reconciler) peerRelayConfigSecret(pr *tsapi.PeerRelay, idx int32, endpoint *tsapi.PeerRelayEndpoint) (*corev1.Secret, error) {
+	labels := peerRelayServiceLabels(pr.Name, idx)
+	return tailscaled.NewConfigSecret(tailscaled.ConfigSecretOptions{
+		Name:      configSecretName(pr.Name, idx),
+		Namespace: r.tailscaleNamespace,
+		Labels:    labels,
+		Config:    peerRelayTailscaledConfig(pr, idx, endpoint),
+	})
+}
+
+func (r *Reconciler) peerRelayStatefulSet(pr *tsapi.PeerRelay, replicas int32) *appsv1.StatefulSet {
+	labels := peerRelayLabels(pr.Name)
+	return tailscaled.NewStatefulSet(tailscaled.StatefulSetOptions{
+		Name:      pr.Name,
+		Namespace: r.tailscaleNamespace,
+		Labels:    labels,
+		Image:     r.proxyImage,
+		Replicas:  replicas,
+		ConfigSecretNameFunc: func(idx int32) string {
+			return configSecretName(pr.Name, idx)
+		},
+	})
+}
