97adfeb7e898273234edfe1de2ff377829b5ae49
Alex Chan <alexc@tailscale.com>
2026-07-27 08:28:28 +0100

    types/persist: add a comment explaining "NetworkLockKey"
    Changing the name to "TailnetLockKey" would be clearer but introduces
    more risk; this is an easy and low-stakes improvement.
    
    Updates tailscale/corp#37904
    
    Change-Id: I38d804202538b8670a80e744eb4dcb689f0002df
    Signed-off-by: Alex Chan <alexc@tailscale.com>

--- types/persist/persist.go
+++ types/persist/persist.go
@@ -24,9 +24,13 @@ type Persist struct {
 	PrivateNodeKey    key.NodePrivate
 	OldPrivateNodeKey key.NodePrivate // needed to request key rotation
 	UserProfile       tailcfg.UserProfile
-	NetworkLockKey    key.NLPrivate
-	NodeID            tailcfg.StableNodeID
-	AttestationKey    key.HardwareAttestationKey `json:",omitzero"`
+
+	// NetworkLockKey is the node's Tailnet Lock private key.
+	// "Network Lock" was the pre-release name for Tailnet Lock.
+	NetworkLockKey key.NLPrivate
+
+	NodeID         tailcfg.StableNodeID
+	AttestationKey key.HardwareAttestationKey `json:",omitzero"`
 
 	// DisallowedTKAStateIDs stores the tka.State.StateID values which
 	// this node will not operate tailnet lock on. This is used to
