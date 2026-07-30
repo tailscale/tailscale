7dc437768e7518fd211fc81146e504cca16d4f90
Jordan Whited <jordan@tailscale.com>
2026-07-30 15:32:11 -0700

    go.mod: bump wireguard-go
    Updates #20590
    
    Signed-off-by: Jordan Whited <jordan@tailscale.com>

--- flake.nix
+++ flake.nix
@@ -173,4 +173,4 @@
     });
   };
 }
-# nix-direnv cache busting line: sha256-YUibqJuLztzc74AD13Xu1on5ECbF7qbF4h3B2FPhuVI=
+# nix-direnv cache busting line: sha256-SeMc9PQ3acMyeFDVQr/lretHtGrBJxhM4sgBg2l4t1w=
