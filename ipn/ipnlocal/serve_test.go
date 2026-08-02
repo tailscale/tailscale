6bfee8ad8e980bd6711ba45acf93b0ae3695c1d0
sjh9714 <163989462+sjh9714@users.noreply.github.com>
2026-08-03 02:55:41 +0900

    ipn/ipnlocal: allow switching service TUN mode
    Serve configs replace the existing configuration. Validate TUN and handler
    mutual exclusion within the incoming service, but do not reject a valid
    replacement based on the previous service mode.
    
    Fixes #20683
    
    Change-Id: If3fb827e3d7c4474636ecfd6f0d9477cabbb3973
    Signed-off-by: sjh9714 <163989462+sjh9714@users.noreply.github.com>

--- ipn/ipnlocal/serve_test.go
+++ ipn/ipnlocal/serve_test.go
@@ -1949,8 +1949,8 @@ func TestValidateServeConfigUpdate(t *testing.T) {
 			wantError: true,
 		},
 		{
-			name:        "tun-mode-with-handlers",
-			description: "Services cannot enable TUN mode if L4 or L7 handlers already exist",
+			name:        "replace-handlers-with-tun-mode",
+			description: "replacing L4 or L7 handlers with TUN mode should work",
 			existing: &ipn.ServeConfig{
 				Services: map[tailcfg.ServiceName]*ipn.ServiceConfig{
 					"svc:foo": {
@@ -1974,11 +1974,11 @@ func TestValidateServeConfigUpdate(t *testing.T) {
 					},
 				},
 			},
-			wantError: true,
+			wantError: false,
 		},
 		{
-			name:        "handlers-with-tun-mode",
-			description: "Services cannot add L4 or L7 handlers if TUN mode is already enabled",
+			name:        "replace-tun-mode-with-handlers",
+			description: "replacing TUN mode with L4 or L7 handlers should work",
 			existing: &ipn.ServeConfig{
 				Services: map[tailcfg.ServiceName]*ipn.ServiceConfig{
 					"svc:foo": {
@@ -2002,7 +2002,7 @@ func TestValidateServeConfigUpdate(t *testing.T) {
 					},
 				},
 			},
-			wantError: true,
+			wantError: false,
 		},
 	}
 
