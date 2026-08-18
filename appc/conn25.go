953634b045659aff847c4e560946497df5b064dd
Fran Bull <fran@tailscale.com>
2026-08-18 09:25:18 -0700

    appc,feature/conn25: move functions
    These functions are no longer called from multiple places and can be
    localized to the package of the caller.
    
    Updates tailscale/corp#46766
    
    Signed-off-by: Fran Bull <fran@tailscale.com>

--- appc/conn25.go
+++ appc/conn25.go
@@ -4,57 +4,16 @@
 package appc
 
 import (
-	"cmp"
 	"fmt"
-	"slices"
 	"strings"
 
-	"tailscale.com/ipn/ipnext"
 	"tailscale.com/tailcfg"
 	"tailscale.com/tailcfg/nodecap"
 	"tailscale.com/types/appctype"
 	"tailscale.com/types/dnstype"
-	"tailscale.com/util/set"
 )
 
 const AppConnectorsExperimentalAttrName = "tailscale.com/app-connectors-experimental"
-
-func isPeerEligibleConnector(peer tailcfg.NodeView) bool {
-	if !peer.Valid() || !peer.Hostinfo().Valid() {
-		return false
-	}
-	isConn, _ := peer.Hostinfo().AppConnector().Get()
-	return isConn
-}
-
-func sortByPreference(ns []tailcfg.NodeView) {
-	// The ordering of the nodes is semantic (callers use the first node they can
-	// get a peer api url for). We don't (currently 2026-02-27) have any
-	// preference over which node is chosen as long as it's consistent.  In the
-	// future we anticipate integrating with traffic steering.
-	slices.SortFunc(ns, func(a, b tailcfg.NodeView) int {
-		return cmp.Compare(a.ID(), b.ID())
-	})
-}
-
-// PickConnector returns peers the backend knows about that match the app, in order of preference to use as
-// a connector.
-func PickConnector(nb ipnext.NodeBackend, app appctype.Conn25Attr) []tailcfg.NodeView {
-	appTagsSet := set.SetOf(app.Connectors)
-	matches := nb.AppendMatchingPeers(nil, func(n tailcfg.NodeView) bool {
-		if !isPeerEligibleConnector(n) {
-			return false
-		}
-		for _, t := range n.Tags().All() {
-			if appTagsSet.Contains(t) {
-				return true
-			}
-		}
-		return false
-	})
-	sortByPreference(matches)
-	return matches
-}
 
 // DNSAddrScheme is the custom URI scheme used for conn25-managed split DNS
 // entries to determine the destination at query time rather than configuration
--- feature/conn25/conn25.go
+++ feature/conn25/conn25.go
@@ -9,6 +9,7 @@ package conn25
 
 import (
 	"bytes"
+	"cmp"
 	"container/list"
 	"context"
 	"encoding/json"
@@ -1080,9 +1081,46 @@ func makePeerAPIReq(ctx context.Context, httpClient *http.Client, urlBase string
 	return nil
 }
 
+func isPeerEligibleConnector(peer tailcfg.NodeView) bool {
+	if !peer.Valid() || !peer.Hostinfo().Valid() {
+		return false
+	}
+	isConn, _ := peer.Hostinfo().AppConnector().Get()
+	return isConn
+}
+
+func sortByPreference(ns []tailcfg.NodeView) {
+	// The ordering of the nodes is semantic (callers use the first node they can
+	// get a peer api url for). We don't (currently 2026-02-27) have any
+	// preference over which node is chosen as long as it's consistent.  In the
+	// future we anticipate integrating with traffic steering.
+	slices.SortFunc(ns, func(a, b tailcfg.NodeView) int {
+		return cmp.Compare(a.ID(), b.ID())
+	})
+}
+
+// pickConnector returns peers the backend knows about that match the app, in order of preference to use as
+// a connector.
+func pickConnector(nb ipnext.NodeBackend, app appctype.Conn25Attr) []tailcfg.NodeView {
+	appTagsSet := set.SetOf(app.Connectors)
+	matches := nb.AppendMatchingPeers(nil, func(n tailcfg.NodeView) bool {
+		if !isPeerEligibleConnector(n) {
+			return false
+		}
+		for _, t := range n.Tags().All() {
+			if appTagsSet.Contains(t) {
+				return true
+			}
+		}
+		return false
+	})
+	sortByPreference(matches)
+	return matches
+}
+
 func (e *extension) pickConnectorURLBase(app appctype.Conn25Attr) (tailcfg.NodeView, string) {
 	nb := e.host.NodeBackend()
-	peers := appc.PickConnector(nb, app)
+	peers := pickConnector(nb, app)
 	var urlBase string
 	var conn tailcfg.NodeView
 	for _, p := range peers {
