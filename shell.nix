69fe345ad9a1c1bd1eed8e9634604d69bb0e573b
Tom Proctor <tomhjp@users.noreply.github.com>
2026-08-19 13:55:19 +0000

    go.mod: bump ebpf, go-git, go-billy and aws-sdk-go-v2 for govulncheck
    Bump five dependencies to resolve the following advisories;
    GO-2026-6238, GO-2026-5764, GO-2026-5597, GO-2026-5490, GO-2026-5496,
    GO-2026-5105.
    
    Updates tailscale/corp#9497
    
    Signed-off-by: Tom Proctor <tomhjp@users.noreply.github.com>

--- shell.nix
+++ shell.nix
@@ -16,4 +16,4 @@
 ) {
   src =  ./.;
 }).shellNix
-# nix-direnv cache busting line: sha256-uwo3oCFKfjRXVEx/xXT2zQrS2pYX32vhVaOAR5NsBOM=
+# nix-direnv cache busting line: sha256-kRkKLGNZwgw26eIzj+tWfYlDoIfX9TF2ril8Kj3KfGE=
