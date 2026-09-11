8054aa2576314650d4866f946e729a182799abd4
Jordan Whited <jordan@tailscale.com>
2026-09-11 12:46:29 -0700

    go.mod: bump wireguard-go dep for small packet bufs pool
    To reduce packet slab memory retained in per-peer staged queues.
    
    Updates tailscale/corp#22467
    Updates tailscale/corp#46716
    
    Signed-off-by: Jordan Whited <jordan@tailscale.com>

--- flake.nix
+++ flake.nix
@@ -173,4 +173,4 @@
     });
   };
 }
-# nix-direnv cache busting line: sha256-xEfkGcKS2G5JzNFHRumnVaoY98d3qU/IELvR6kk8l7s=
+# nix-direnv cache busting line: sha256-lN0JafHPgjNSrYYXdlnLtQoHVrfR3a6EqwE7AsFq1+A=
